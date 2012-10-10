45cf2ae47d129bd4c1be0bec25ce1370cc940be3
Michael McGuinness <mmcguinn@cs.uml.edu>
2012-10-10 16:49:12 -0400

    -Added user validation emails. Right now they are only sent on user creation if they have an email. -Changed the user model to require a username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,24 @@ class User < ActiveRecord::Base
   attr_accessible :content, :email, :firstname, :group_id, :lastname, :password, :password_confirmation, :username, :validated
   
   validates_uniqueness_of :email, case_sensitive: false, if: :email?
-  validates_uniqueness_of :username, case_sensitive: false, if: :username?
+  validates_uniqueness_of :username, case_sensitive: false
+  validates_presence_of :username
   
   has_secure_password
 
-  validate :has_credentials
+  before_create :check_validation
+  after_create :check_and_send_validation
 
   private
-  def has_credentials
-    if email.blank? and username.blank?
-      errors.add :base, "You must have a username and/or email address"
+  def check_validation
+    if (not validated) and (not email.blank?)
+      self.validation_key = BCrypt::Password::create(email)
+    end
+  end
+  
+  def check_and_send_validation
+    if (not validated) and (not validation_key.blank?)
+      UserMailer.validation_email(self)
     end
   end
 end
