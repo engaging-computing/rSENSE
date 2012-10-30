35d725b6b8e9df01f9cd62ec6db38138fb23932f
jdalphon <jdalphon@jimbo.uml.edu>
2012-10-30 16:23:15 -0400

    Changed the layout of the profile page, now has media. Changed the default names of media objects

--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -1,3 +1,7 @@
 class MediaObject < ActiveRecord::Base
   attr_accessible :experiment_id, :media_type, :name, :session_id, :src, :user_id
+  
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
+  belongs_to :experiment, class_name: "Experiment", foreign_key: "experiment_id" 
+  belongs_to :session, class_name: "ExperimentSession", foreign_key: "session_id" 
 end
