1130699d5b4c9146cc25c065946488baed19d904
Eric Fairbanks <eric.p.fairbanks@gmail.com>
2012-11-07 15:18:11 -0500

    my changes thus far

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -10,4 +10,12 @@ class Experiment < ActiveRecord::Base
 
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+  
 end
