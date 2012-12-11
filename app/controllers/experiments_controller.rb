32bf0a23f2579b22b45c7db4e442b2d81467ca7f
Hassan McKusick <Hassan.McKusick@gmail.com>
2012-12-11 17:30:54 -0500

    Yay real data

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -21,23 +21,23 @@ class ExperimentsController < ApplicationController
   # GET /experiments/1.json
   def show
     @experiment = Experiment.find(params[:id])
-		
-		#Determine if the experiment is cloned
-		@cloned_experiment = nil
+    
+    #Determine if the experiment is cloned
+    @cloned_experiment = nil
     if(!@experiment.cloned_from.nil?)
-			@cloned_experiment = Experiment.find(@experiment.cloned_from)
-		end
-		
-		#Get number of likes
-		@likes = @experiment.likes.count
-		
-		@liked_by_cur_user = false
-		if(Like.find_by_user_id_and_experiment_id(@cur_user,@experiment.id)) 
-			@liked_by_cur_user = true
-		end
-	
-		
-		respond_to do |format|
+      @cloned_experiment = Experiment.find(@experiment.cloned_from)
+    end
+    
+    #Get number of likes
+    @likes = @experiment.likes.count
+    
+    @liked_by_cur_user = false
+    if(Like.find_by_user_id_and_experiment_id(@cur_user,@experiment.id)) 
+      @liked_by_cur_user = true
+    end
+
+    
+    respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @experiment }
     end
@@ -69,20 +69,20 @@ class ExperimentsController < ApplicationController
   # POST /experiments.json
   def create
     #@experiment = Experiment.new(params[:experiment])
-		
-		if(params[:experiment_id])
-			@tmp_exp = Experiment.find(params[:experiment_id])
-			@experiment = Experiment.new({user_id: @cur_user.id, title:"#{@tmp_exp.title} (clone)", content: @tmp_exp.content, filter: @tmp_exp.filter, cloned_from:@tmp_exp.id})
-			success = @experiment.save
-			@tmp_exp.fields.all.each do |f|
-				Field.create({experiment_id:@experiment.id, field_type: f.field_type, name: f.name, unit: f.unit})
-			end
-		else
-			@experiment = Experiment.new({user_id: @cur_user.id, title:"#{@cur_user.firstname} #{@cur_user.lastname[0].pluralize} Experiment"})
-			success = @experiment.save
-		end
+    
+    if(params[:experiment_id])
+      @tmp_exp = Experiment.find(params[:experiment_id])
+      @experiment = Experiment.new({user_id: @cur_user.id, title:"#{@tmp_exp.title} (clone)", content: @tmp_exp.content, filter: @tmp_exp.filter, cloned_from:@tmp_exp.id})
+      success = @experiment.save
+      @tmp_exp.fields.all.each do |f|
+        Field.create({experiment_id:@experiment.id, field_type: f.field_type, name: f.name, unit: f.unit})
+      end
+    else
+      @experiment = Experiment.new({user_id: @cur_user.id, title:"#{@cur_user.firstname} #{@cur_user.lastname[0].pluralize} Experiment"})
+      success = @experiment.save
+    end
 
-		respond_to do |format|
+    respond_to do |format|
       if success
         format.html { redirect_to @experiment, notice: 'Experiment was successfully created.' }
         format.json { render json: @experiment, status: :created, location: @experiment }
@@ -123,25 +123,34 @@ class ExperimentsController < ApplicationController
   
   def addExperimentSession
     @experiment = Experiment.find(params[:id])
-    #@experiment_session = ExperimentSession.new(:user_id => @cur_user.id, :experiment_id => @experiment.id, :title => "#{@cur_user.name}'s Session")
 
     header = params[:ses_info][:header]
     data = params[:ses_info][:data]
 
     if !data.nil?
+     
+      @experiment_session = ExperimentSession.create(:user_id => @cur_user.id, :experiment_id => @experiment.id, :title => "#{@cur_user.name}'s Session")
 
       mongo_data = []
-      row = []
       
       data.each do |dp|
-        header.each do |h|
-          id = h[1]["id"]
-          row << { id => dp }
+        row = []
+        header.each_with_index do |field, col_index|
+          row << { field[1][:id] => dp[1][col_index] }
         end
         mongo_data << row
       end
+            
+      data_to_add = DataSet.new(:experiment_session_id => @experiment_session.id, :data => mongo_data)    
       
-      response = data
+      followURL = url_for :controller => :experiment_sessions, :action => :show, :id => @experiment_session
+      
+      if data_to_add.save!
+        response = { status: 'success', follow: followURL }
+      else
+        response = { status: 'fail' }
+      end 
+      
     else
       response = ["No data"]
     end
@@ -151,36 +160,36 @@ class ExperimentsController < ApplicationController
       format.json { render json: response }
     end    
   end
-	
-	def updateLikedStatus
-		
-		like = Like.find_by_user_id_and_experiment_id(@cur_user,params[:id])
-	
-		if(like)
-			Like.destroy(like.id)
-		else
-			Like.create({user_id:@cur_user.id,experiment_id:params[:id]})
-		end
+
+  def updateLikedStatus
     
-		count = Experiment.find(params[:id]).likes.count
-		
-		if(count == 0 || count > 1)
-			@response = count.to_s + " people liked this"
-		else
-			@response = count.to_s + " person liked this"
-		end
-		
-		respond_to do |format|
+    like = Like.find_by_user_id_and_experiment_id(@cur_user,params[:id])
+
+    if(like)
+      Like.destroy(like.id)
+    else
+      Like.create({user_id:@cur_user.id,experiment_id:params[:id]})
+    end
+    
+    count = Experiment.find(params[:id]).likes.count
+    
+    if(count == 0 || count > 1)
+      @response = count.to_s + " people liked this"
+    else
+      @response = count.to_s + " person liked this"
+    end
+    
+    respond_to do |format|
       format.json { render json: {update: @response} }
     end
-	end
-	
-	
-	def manualEntry
-		@experiment = Experiment.find(params[:id])
-		logger.info "============================================================================="
-	end
-	
+  end
+
+
+  def manualEntry
+    @experiment = Experiment.find(params[:id])
+    logger.info "============================================================================="
+  end
+
   ## POST /experiment_sessions/1
   def postCSV
 
