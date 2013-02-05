27059475df1dc2ec9a661dfccaa961a28f9422fe
jdalphon <jdalphon@jdalphon-dev.(none)>
2013-02-05 15:48:26 -0500

    Adding the ability to change tutorials on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,15 @@ class HomeController < ApplicationController
       @experiments.each do |e|
         e['owner'] = User.find(e.user_id) 
       end
-  
+      
+      tutorials = Tutorial.where("featured_number IS NOT NULL")
+      
+      @tutorials = []
+      @tutorials[1] = tutorials.where('featured_number == ?',1).first || nil
+      @tutorials[2] = tutorials.where('featured_number == ?',2).first || nil
+      @tutorials[3] = tutorials.where('featured_number == ?',3).first || nil
+      @tutorials[4] = tutorials.where('featured_number == ?',4).first || nil
+      
+      @all_tutorials = Tutorial.find(:all)
   end
 end
