6a4ad1c55b6cec1a7d0dfec0923285d02e294bfb
jdalphon <jdalphon@jdalphon-dev.(none)>
2013-02-06 14:18:16 -0500

    Made the titles link to the tutorials. Made it so tutorials can have many media_objects necessary for images

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,5 +2,7 @@ class Tutorial < ActiveRecord::Base
   attr_accessible :content, :title, :featured_number
   
   validates_uniqueness_of :featured_number, :allow_nil => true
+  has_many :media_objects
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
   
 end
