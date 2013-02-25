16725bb2dee221f7b72b375089aa653c44a915b8
Evana Gizzi <evanagizzi@evana-gizzis-macbook-pro.local>
2013-02-25 11:32:50 -0500

    commits from friday

--- app/controllers/experiment_templates_controller.rb
+++ app/controllers/experiment_templates_controller.rb
@@ -18,6 +18,37 @@ class ExperimentTemplatesController < ApplicationController
     end
   end
 
+  
+  #Featured list
+  @featured_3 = Experiment.where(featured: true).order("updated_at DESC").limit(3);
+  
+  jsonExperiments = []
+  
+  @experiments.each do |exp|
+    
+    newJsonExperiment = {}
+    
+    newJsonExperiment["title"]          = exp.title
+    newJsonExperiment["timeAgoInWords"] = time_ago_in_words(exp.created_at)
+    newJsonExperiment["createdAt"]      = exp.created_at.strftime("%B %d, %Y")
+    newJsonExperiment["featured"]       = exp.featured
+    newJsonExperiment["ownerName"]      = "#{exp.owner.firstname} #{exp.owner.lastname}"
+    newJsonExperiment["experimentPath"] = experiment_path(exp)
+    newJsonExperiment["ownerPath"]      = user_path(exp.owner)
+    newJsonExperiment["filters"]        = exp.filter
+    
+    if(exp.featured_media_id != nil) 
+      newJsonExperiment["mediaPath"] = MediaObject.find_by_id(exp.featured_media_id).src;
+    end
+    
+    jsonExperiments = jsonExperiments << newJsonExperiment
+    
+  end
+  
+  respond_to do |format|
+    format.html
+    format.json { render json: jsonExperiments }
+  end
 
 
 end
