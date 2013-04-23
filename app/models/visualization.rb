8b3d0ac7f1fc711253f47818c6f9d05ffcb5e52d
jdalphon <jdalphon@jdalphon-dev.(none)>
2013-04-23 13:23:47 -0400

    Adding the ability to feature visualizations along with the time of feature. Adding an embeded vis to the home page

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -1,5 +1,5 @@
 class Visualization < ActiveRecord::Base
-  attr_accessible :content, :data, :project_id, :globals, :title, :user_id
+  attr_accessible :content, :data, :project_id, :globals, :title, :user_id, :featured, :featured_at
 
   validates_presence_of :title
   validates_presence_of :user_id
@@ -16,5 +16,4 @@ class Visualization < ActiveRecord::Base
         scoped
     end
   end
-  
 end
