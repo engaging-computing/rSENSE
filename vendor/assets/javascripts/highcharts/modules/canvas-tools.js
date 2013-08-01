b2441fef4a00117327390da20812f55d8b722e0a
mmcguinn <mmcguinn@cs.uml.edu>
2013-08-01 10:40:09 -0400

    -Updated highcharts to 3.0.3

--- vendor/assets/javascripts/highcharts/modules/canvas-tools.js
+++ vendor/assets/javascripts/highcharts/modules/canvas-tools.js
@@ -11,7 +11,7 @@
 
  Requires: rgbcolor.js - http://www.phpied.com/rgb-color-parser-in-javascript/
 
- Highcharts JS v3.0.2 (2013-06-05)
+ Highcharts JS v3.0.3 (2013-07-31)
  CanVGRenderer Extension module
 
  (c) 2011-2012 Torstein Hønsi, Erik Olsson
