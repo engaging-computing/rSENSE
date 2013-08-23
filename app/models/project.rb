a15f508503ed6c8d291f27fe8a79ddaa2b519b8f
mmcguinn <mmcguinn@cs.uml.edu>
2013-08-23 15:59:08 -0400

    Revert "Copyedit to /users/show.js.coffee. Addressed #557 in projects/index.js.coffee and models/project.rb. Removed code from manualEntry.html.erb regarding units which broke everything. #589 has be reopened."
    This reverts commit fe2a71f2e8475d9aef92ef07963fc9a92b6b3cae. (for #589)

--- app/models/project.rb
+++ app/models/project.rb
@@ -50,13 +50,6 @@ class Project < ActiveRecord::Base
     end
   end
   
-  def all_hidden
-    @data_sets = data_sets.where( 'hidden == ?', false )
-    if @data_sets.empty?
-      return true
-    end
-  end
-  
   def to_hash(recurse = true)
     h = {
       id: self.id,
@@ -64,7 +57,6 @@ class Project < ActiveRecord::Base
       name: self.name,
       url: UrlGenerator.new.project_url(self),
       hidden: self.hidden,
-      allHidden: self.all_hidden,
       featured: self.featured,
       likeCount: self.like_count,
       timeAgoInWords: time_ago_in_words(self.created_at),
