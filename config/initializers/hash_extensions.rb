442d5f924e502f9eec6e0d90271dba4b472b3adb
Nicholas Ver Voort <moldorma@gmail.com>
2013-08-27 16:25:17 -0400

    fixed indentation

--- config/initializers/hash_extensions.rb
+++ config/initializers/hash_extensions.rb
@@ -1,16 +1,16 @@
 # Helper method for seperating hashes.
 # Removes the keys from hs that are in ls and returns them in a new hash
 class Hash
-def extract_keys! (ls)
-res = self.class.new
+  def extract_keys! (ls)
+    res = self.class.new
+    
+    ls.each do |k|
+      if self.include? k
+        res[k] = self[k]
+        self.delete k
+      end
+    end
 
-ls.each do |k|
-if self.include? k
-res[k] = self[k]
-self.delete k
-end
-end
-
-res
-end
+  res
+  end
 end
