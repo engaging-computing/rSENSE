e040196594324360121cae00405d28b34259cb5c
Nat Tuck <nat@ferrus.net>
2013-09-16 10:35:39 -0400

    Test display of existing dataset.

--- test/integration/enter_data_set_test.rb
+++ test/integration/enter_data_set_test.rb
@@ -24,7 +24,7 @@ class EnterDataSetTest < ActionDispatch::IntegrationTest
     click_on 'Manual Entry'
     find('#manualTable').all('.input-small')[0].native.send_keys "5"
     find('#manualTable').all('.input-small')[1].native.send_keys "6"
-    find('#manualTable').all('.input-small')[1].native.send_keys :enter
+    click_on "Add Row"
     find('#manualTable').all('.input-small')[2].native.send_keys "22"
     find('#manualTable').all('.input-small')[3].native.send_keys "1"
     click_on 'Save'
