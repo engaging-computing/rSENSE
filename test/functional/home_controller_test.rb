01e2fb66808f03dc53d441cf10e9d01228d17cb2
Nat Tuck <nat@ferrus.net>
2013-09-25 14:36:37 -0400

    Functional tests for methods in home_controller

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -6,4 +6,13 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
 end
