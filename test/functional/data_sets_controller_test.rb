0cbc5a63beb75e3292c1b63d762a4df0e045a818
Nat Tuck <nat@ferrus.net>
2013-09-25 10:46:17 -0400

    Add test for data_sets/new.json

--- test/functional/data_sets_controller_test.rb
+++ test/functional/data_sets_controller_test.rb
@@ -5,7 +5,19 @@ class DataSetsControllerTest < ActionController::TestCase
     @kate = users(:kate)
     @data_set = data_sets(:one)
     @tgd = data_sets(:thanksgiving)
-   end
+  end
+
+  test "should redirect to viz for show data set" do
+    get :show, { id: @tgd.id }
+    assert_response :redirect 
+  end
+ 
+  test "should get dataset data" do
+    get :show, { id: @tgd.id, format: 'json', recur: 'true' }, { user_id: @kate }
+    assert_response :success
+    ds = JSON.parse(@response.body)
+    assert_equal 3, ds['data'][0].keys.length, "Actually got records in data"
+  end
 
   test "should create data_set" do
     assert_difference('DataSet.count') do
@@ -16,22 +28,25 @@ class DataSetsControllerTest < ActionController::TestCase
     assert_redirected_to data_set_path(assigns(:data_set))
   end
 
+  test "should create data_set and get JSON response" do
+    assert_difference('DataSet.count') do
+      post :create, { format: 'json', data_set: { content: @data_set.content, 
+        project_id: @data_set.project_id, title: @data_set.title, 
+        user_id: @data_set.user_id }}, { user_id: @kate }
+    end
+
+    ds = JSON.parse(@response.body)
+    assert_equal ds['name'], @data_set.title, "Actually saved data"
+
+    assert_response :success
+  end
+
+
+
   test "should update data_set" do
     put :update, { id: @data_set, data_set: { content: @data_set.content, project_id: @data_set.project_id, 
       title: @data_set.title, user_id: @data_set.user_id }}, { user_id: @kate }
     assert_redirected_to data_set_path(assigns(:data_set))
-  end
-
-  test "should redirect to viz for show data set" do
-    get :show, { id: @tgd.id }
-    assert_response :redirect 
-  end
-
-  test "should get dataset data" do
-    get :show, { id: @tgd.id, format: 'json', recur: 'true' }, { user_id: @kate }
-    assert_response :success
-    ds = JSON.parse(@response.body)
-    assert_equal 3, ds['data'][0].keys.length, "Actually got records in data"
   end
 
   test "should destroy data_set" do
