51bfc7ac7c87789a56be5931be8a55167a35123d
Nat Tuck <nat@ferrus.net>
2013-10-17 16:49:33 -0400

    Add password reset functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -70,8 +70,8 @@ Rsense::Application.routes.draw do
   match '/sessions/verify' => 'sessions#verify'
 
   match "/users/pw_request" => "users#pw_request"
-  match "/users/pw_send" => "users#pw_send"
-  match "/users/pw_form" => "users#pw_form"
+  match "/users/pw_send_key" => "users#pw_send_key"
+  match "/users/pw_reset/:key" => "users#pw_reset"
   resources :users
   match "/users/validate/:key" => "users#validate"
   match "/users/:id/contributions" => "users#contributions"
