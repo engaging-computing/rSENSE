aff98e693fce62063cddfea4076eedb3693942e6
mmcguinn <mmcguinn@cs.uml.edu>
2013-10-18 13:07:02 -0400

    Revert "Clean up email address validation."
    This reverts commit 087544834005dcafd2ae0eba1dd77eca7438c2f8.

--- config/routes.rb
+++ config/routes.rb
@@ -65,9 +65,7 @@ Rsense::Application.routes.draw do
 
   match '/sessions/verify' => 'sessions#verify'
 
-  match "/users/pw_request" => "users#pw_request"
-  match "/users/pw_send" => "users#pw_send"
-  match "/users/pw_form" => "users#pw_form"
+  match "/users/verify" => "users#verify"
   resources :users
   match "/users/validate/:key" => "users#validate"
   match "/users/:id/contributions" => "users#contributions"
