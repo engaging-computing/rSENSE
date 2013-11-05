ebafaf6429b1b8757e3633ebadc629a1cca48214
mmcguinn <mmcguinn@cs.uml.edu>
2013-11-05 17:34:40 -0500

    -Rewrote login system to use a static form page. Removed old JS method.

--- config/routes.rb
+++ config/routes.rb
@@ -59,10 +59,10 @@ Rsense::Application.routes.draw do
   match "/media_objects/saveMedia/*keys" => "media_objects#saveMedia"
 
   controller :sessions do
+    get 'login' => :new
     post 'login' => :create
     delete 'login' => :destroy
   end
-
   match '/sessions/verify' => 'sessions#verify'
 
   match "/users/pw_request" => "users#pw_request"
