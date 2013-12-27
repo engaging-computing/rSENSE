01e93b1ab6313df51dd788edd06f035eea7d5684
Nat Tuck <nat@ferrus.net>
2013-12-27 16:48:29 -0500

    Clean up minimum version to just MAJOR >= 2

--- test/unit/meta_test.rb
+++ test/unit/meta_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class DataSetTest < ActiveSupport::TestCase
-  test "minimum ruby version" do
+  test "minimum ruby version: 2.0.0" do
     vs = RUBY_VERSION.split('.').map {|n| n.to_i}
     assert vs[0] >= 2, "ruby major version"
-    assert vs[1] >= 0, "ruby minor version"
-    assert vs[2] >= 0, "ruby minorer version"
-    assert RUBY_PATCHLEVEL >= 0, "ruby patchlevel"
   end
 
   test "imagemagick is installed" do
