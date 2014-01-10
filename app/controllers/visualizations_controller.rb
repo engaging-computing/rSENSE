bc13a7f64d00acdd71d673a5ef39e7336b39fc6e
mmcguinn <mmcguinn@cs.uml.edu>
2014-01-10 14:39:06 -0500

    -Removed log.

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -255,9 +255,6 @@ class VisualizationsController < ApplicationController
     @datasets.each do |dataset|
       photos = dataset.media_objects.keep_if{|mo| mo.media_type=="image"}.map{|mo| mo.to_hash(true)}
       hasPics = true if photos.size > 0
-      logger.info "------------------------------------------------------------"
-      logger.info photos
-      logger.info "------------------------------------------------------------"
       metadata[i] = { name: dataset.title, user_id: dataset.user_id, dataset_id: dataset.id, timecreated: dataset.created_at, timemodified: dataset.updated_at, photos: photos}
       dataset.data.each do |row|
         unless row.class == Hash
