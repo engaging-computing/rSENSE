7c8dee3cc5474e075d007b52b4d70d92aa641ccf
jdalphon <james.dalphond@gmail.com>
2014-02-03 17:14:06 -0500

    Updating API

---
+++ app/controllers/api/v1/fields_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class FieldsController < ActionController::FieldsController
+      skip_before_filter :authorise
+      before_filter :set_user, :only => [:create]
+
+      def show
+        super
+      end
+      
+      def create
+        super
+      end
+      
+    end
+  end
+end
---
+++ app/controllers/api/v1/media_objects_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class MediaObjectsController < ActionController::MediaObjectsController
+
+    end
+  end
+end
