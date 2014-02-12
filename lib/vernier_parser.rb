67d4bc1f72898223365961fd43dda05051a90025
Hassan McKusick <hassan.mckusick@gmail.com>
2014-02-12 11:28:17 -0500

    save the changes

--- lib/vernier_parser.rb
+++ lib/vernier_parser.rb
@@ -12,17 +12,66 @@ class VernierParser
     
     @doc = Nokogiri::HTML(@xml)
     
-    @cols = []
+    @col = []
     
     @doc.css('document dataset columncells').each do |col|
-      @cols.push col.children.to_s.split( /\r?\n/ )
+      @col.push col.children.to_s.split( /\r?\n/ )
     end
+    
+    self.rotate
+    
+    self.data
     
   end
   
   def data
-    return @cols
+    @row
   end
   
+  def rotate
+    
+    row = []
+    
+    @col[0].each_with_index do |col_row, row_index|
+    
+      new_row = []
+      
+      @col.each_with_index do |cur, cur_index|
+      
+        new_row.push @col[cur_index][row_index]
+      
+      end
+      
+      row.push new_row
+      
+    end
+    
+    @row = row
+    
+  end
+  
+  def row( row_id )
+    if row_id >= 0 and row_id < self.last_column
+      return @row[row_id]
+    else
+      return []
+    end
+  end
+  
+  def column( col_id )
+    if col_id >= 0 and col_id < self.last_row
+      return @col[col_id]
+    else
+#       return []
+    end
+  end
+  
+  def last_column
+    return @col.size()
+  end
+  
+  def last_row
+    return @row.size()
+  end
 
 end
