e26033c98fc4c5f0f28bd120f7c4b6ce23a56243
jdalphon <james.dalphond@gmail.com>
2014-03-05 16:59:36 -0500

    Starting to add append to dataset for api.

--- app/controllers/api/v1/data_sets_controller.rb
+++ app/controllers/api/v1/data_sets_controller.rb
@@ -16,6 +16,34 @@ module Api
       def jsonDataUpload
         super
       end
+      
+      def append
+        dataset = DataSet.find_by_id(params[:id])
+        newdata = params[:data]
+        if dataset && !newdata.nil?
+          DataSet.transaction do
+            project = Project.find_by_id(dataset.project.id)
+            uploader = FileUploader.new
+            sane = uploader.sanitize_data(newdata)
+            if sane[:status]
+              newdata = uploader.swap_columns(sane[:data_obj],project)
+              dataset.update_attributes(data: dataset.data.concat(newdata))
+           
+              respond_to do |format|
+                format.json { render json: dataset, status: :ok}
+              end
+            else
+              respond_to do |format|
+                format.json { render json: {msg: sane[:msg]}, status: :unprocessable_entity}
+              end
+            end
+          end
+        else
+          respond_to do |format|
+            format.json { render json: {}, status: :not_found } 
+          end
+        end
+      end
     end
   end
 end
