f3a6be65c634aa01231b0088d0eed5d16813f857
Jake Kinsman <jakekinsman@ubuntu.(none)>
2014-05-15 16:31:10 -0400

    Dead Branchhhh

--- test/integration/contrib_key_sorting_test.rb
+++ test/integration/contrib_key_sorting_test.rb
@@ -11,5 +11,17 @@ class ContribKeySortingTest < ActionDispatch::IntegrationTest
   teardown do
     finish
   end
+  
+  #setup
+  setup do
+    
+    visit('/')
+    login('jkinsman1986@gmail.com', 'G1adiator')
+    click_on 'Projects'
+    click_on 'Create Project'
+    find('#project_title').set('Contributor Key Test Project')
+    click_on 'Submit'
+    assert page.has_content? 'Visualizations'
+  end
 
 end
