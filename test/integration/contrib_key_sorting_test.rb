56a48be393b9a7f1fff55e3913a8f73c2374bd2c
Jake Kinsman <jakekinsman@ubuntu.(none)>
2014-05-15 15:49:59 -0400

    Integration test for Contrib Key Sorting in progress!

---
+++ test/integration/contrib_key_sorting_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ContribKeySortingTest < ActionDispatch::IntegrationTest
+  include CapyHelper
+
+  setup do
+    Capybara.current_driver = :webkit
+    Capybara.default_wait_time = 15
+  end
+
+  teardown do
+    finish
+  end
+
+end
