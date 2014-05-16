055206e8425d58e8076cb03db8a1133f2ee9f13c
Jake Kinsman <jakekinsman@ubuntu.(none)>
2014-05-16 07:43:31 -0400

    Intermediate, almost works! (just need to test API)

--- test/integration/contrib_key_with_data_set_test.rb
+++ test/integration/contrib_key_with_data_set_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class ContribKeyWithDataSetTest < ActionDispatch::IntegrationTest
   include CapyHelper
 
@@ -19,7 +18,20 @@ class ContribKeyWithDataSetTest < ActionDispatch::IntegrationTest
     click_on 'Create Project'
     find('#project_title').set('Contributor Key Test Project')
     click_on 'Create'
-    assert page.has_content? 'Project was successfully created.'
+    #post '/api/v1/projects',
+
+   #       project_name: 'Contributor Key Test Project',
+   #       email: 'kcarcia@cs.uml.edu',
+   #       password: '12345'
+    
+    #assert_response :success
+    #puts response
+    #assert page.has_content? 'Login'
+    #assert page.has_content? 'Register'
+    #login('kcarcia@cs.uml.edu', '12345')
+    #visit '/'
+    #click_on 'Projects'
+    #click_on 'Contributor Key Test Project'
     find('#manual_fields').click
     find('#new_field').find(:xpath, 'option[2]').select_option
     assert page.has_content? 'Field added'
@@ -42,19 +54,39 @@ class ContribKeyWithDataSetTest < ActionDispatch::IntegrationTest
     click_on 'Back to Project'
     click_on 'Logout'
     assert page.has_content? 'Login'
-  end
-  test "upload_data" do
-    visit '/'
+    visit '/contrib_keys/clear'
     click_on 'Projects'
     click_on 'Contributor Key Test Project'
     find('#key').set('test2')
     click_on 'Submit Key'
-    visit '#{request.request_uri}/manualEntry'
+    click_on 'Manual Entry'
     find('#data_set_name').set('Data1')
     find('#contrib_name').set('Jake')
     find('.validate_number').set('5')
     click_on 'Save'
     assert page.has_content? 'Visualizations'
     click_on 'Contributor Key Test Project'
+    assert page.has_css? '.key', 'Key should have been displayed.'
+    visit '/contrib_keys/clear'
+    click_on 'Projects'
+    click_on 'Contributor Key Test Project'
+    find('#key').set('test1')
+    click_on 'Submit Key'
+    click_on 'Manual Entry'
+    find('#data_set_name').set('Data2')
+    find('#contrib_name').set('Jake')
+    find('.validate_number').set('5')
+    click_on 'Save'
+    assert page.has_content? 'Visualizations'
+    click_on 'Contributor Key Test Project'
+    assert page.has_content? 'Data Sets'
+    assert page.has_content? 'Contribute Data'
+    assert_equal page.all( :css, '.key')[0][:title], 'test1'
+    assert_equal page.all( :css, '.key')[1][:title], 'test2'
+    temp = page.all( :css, '.key')
+    assert_not_equal( temp[0].find('a').text , temp[1].find('a').text )
+    visit '/contrib_keys/clear'  
+
   end
+
 end
