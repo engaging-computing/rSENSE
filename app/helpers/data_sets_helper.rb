640d3c8f9ba8be8f28eac835fc7043243a7d737f
Hassan McKusick <Hassan.McKusick@gmail.com>
2014-06-18 14:23:39 -0400

    All the helpers that called shared/edit_info.html had to be updated to have the href variable

--- app/helpers/data_sets_helper.rb
+++ app/helpers/data_sets_helper.rb
@@ -1,6 +1,6 @@
 module DataSetsHelper
-  def data_set_edit_helper(field, can_edit = false, make_link = true)
+  def data_set_edit_helper(field, can_edit = false, make_link = true, url = '#')
     render 'shared/edit_info', type: 'data_set', field: field, value: @data_set[field],
-      row_id: @data_set.id, can_edit: can_edit, make_link: make_link
+      row_id: @data_set.id, can_edit: can_edit, make_link: make_link, href: url
   end
 end
