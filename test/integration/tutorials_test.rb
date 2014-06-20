b7ddb136ed3fbe2b6ed8794f68d660a2cad89e2b
Nat Tuck <nat@ferrus.net>
2014-06-20 19:13:39 -0400

    Fix some tests.

--- test/integration/tutorials_test.rb
+++ test/integration/tutorials_test.rb
@@ -35,8 +35,7 @@ class TutorialsTest < ActionDispatch::IntegrationTest
     visit '/tutorials'
     assert page.has_content?('Awesome Tutorial'), 'Tutorial should have been published'
 
-    page.execute_script('$($("span:contains(Awesome Tutorial)").closest("div.item-desc").find("a"))[0].click()')
-    # click_on 'Awesome Tutorial'
+    click_on 'Awesome Tutorial'
     assert page.has_content?('Publish'), 'Should have ended up on tutorials show page'
     find('#publish_tutorial').click
 
