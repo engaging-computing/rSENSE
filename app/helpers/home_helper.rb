2ae75a82a147af97cc2a7f34363e3e404a332341
Jake Kinsman <jakekinsman@ubuntu.(none)>
2014-07-11 14:46:01 -0400

    Think we're done!

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,8 +4,6 @@ module HomeHelper
   def mobileParse(i)
     if Nokogiri.HTML(i.content).search('p').length != 0
       first_paragraph = Nokogiri.HTML(i.content).search('p').first
-      puts "PRINTING FIRST PARAGRAPH:\n"
-      puts first_paragraph.inspect
       raw(first_paragraph.text)
     else
       ''
