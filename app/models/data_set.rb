5390814645064820b80f54adc7bfa74cf48e398b
Bobby Donald <bobbydonald25@gmail.com>
2014-07-16 15:51:06 -0400

    removed comma

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -55,7 +55,7 @@ class DataSet < ActiveRecord::Base
       fieldCount: project.fields.length,
       datapointCount: data.length,
       displayURL: "/projects/#{project.id}/data_sets/#{id}",
-      data: data,
+      data: data
     }
 
     if recurse
