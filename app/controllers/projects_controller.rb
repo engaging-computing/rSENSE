7b645d41294a6c803f0a52207aec98eb5b881d74
Nat Tuck <nat@ferrus.net>
2014-07-21 15:41:25 -0400

    Clone should copy description.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -344,6 +344,7 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @clone = Project.new
     @clone.title = @project.title + ' (clone)'
+    @clone.content = @project.content
     @cloned_project = @project
     respond_to do |format|
       format.html
