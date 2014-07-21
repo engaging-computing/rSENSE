7c23c01e5caa9af694a783c2822f5c69ab27e6a3
Anthony Salani <asalani93@gmail.com>
2014-07-21 14:05:00 -0400

    added colorpicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,7 +41,7 @@
 //= require jquery.prettyPhoto
 //= require markerclustererplus
 //= require oms
-//= require bootstrap-colorpicker
+//= require isense-colorpicker
 //
 //= require highvis/savedVis
 //= require highvis/visUtils
