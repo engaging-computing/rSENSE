cec956d7a0c7ea4cfa5f3a1a45c48a9bcba90006
Bobby Donald <bobbydonald25@gmail.com>
2014-08-06 11:26:14 -0400

    fixed errors in rake test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -93,7 +93,9 @@ class ApplicationController < ActionController::Base
       project = Project.find_by_id(data_set.project_id)
       key = project.contrib_keys.find_by_key(params[:contribution_key])
       if project && !key.nil? && data_set.key == key.name
-        unless params.key? :contributor_name
+        if params.key? :contributor_name
+          @cur_user = User.find_by_id(project.owner.id)
+        else
           respond_to do |format|
             format.json { render json: { msg: 'Missing contributor name' }, status: :unprocessable_entity }
           end
@@ -107,7 +109,9 @@ class ApplicationController < ActionController::Base
     elsif (params.key? :contribution_key) && (['jsonDataUpload', 'saveMedia'].include? params[:action])
       project = Project.find_by_id(params[:id] || params[:pid])
       if project && !project.contrib_keys.find_by_key(params[:contribution_key]).nil?
-        unless params.key? :contributor_name
+        if params.key? :contributor_name
+          @cur_user = User.find_by_id(project.owner.id)
+        else
           respond_to do |format|
             format.json { render json: { msg: 'Missing contributor name' }, status: :unprocessable_entity }
           end
