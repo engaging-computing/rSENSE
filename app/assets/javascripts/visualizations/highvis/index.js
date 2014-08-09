1b0e2da2da523744d3f2298e93c02a1ce2fcd190
Jeremy Poulin <jaypoulz@gmail.com>
2014-08-09 12:15:23 -0700

    Specified the bare minimum load order for vis and saved vises to work properly

---
+++ app/assets/javascripts/visualizations/highvis/index.js
@@ -0,0 +1,8 @@
+// This is a manifest file that'll specifies the order in which these files will
+// be compiled into application.js, which will include all the files listed
+// below.
+//
+//= require ./highmodifiers
+//= require ./visUtils
+//= require ./baseVis
+//= require ./scatter
