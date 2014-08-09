2b2fbb86d67816ba9bcda9f131c7d72cf49a4c7a
Jeremy Poulin <jaypoulz@gmail.com>
2014-08-09 15:59:18 -0700

    Added explanations for the ordering of the files

--- app/assets/javascripts/visualizations/highvis/index.js
+++ app/assets/javascripts/visualizations/highvis/index.js
@@ -2,8 +2,25 @@
 // be compiled into application.js, which will include all the files listed
 // below. This is the bare minimum load order necessary pipeline to work.
 //
+//  Saved vises take precedence
 //= require ./savedVis
+//
+// These two provide the foundation for baseVis
 //= require ./highmodifiers
 //= require ./visUtils
+//
+// BaseVis holds up the other vises
 //= require ./baseVis
+//
+// Scatter is the basis for Timeline
 //= require ./scatter
+//
+// All other vises must be included before runtime
+//= require ./disabledVis
+//= require ./map
+//= require ./timeline
+//= require ./bar
+//= require ./histogram
+//= require ./table
+//= require ./summary
+//= require ./photos
