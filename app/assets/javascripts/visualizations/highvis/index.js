571e8b38ceeaf3c0872ca808a081ade27653b613
Jeremy Poulin <jaypoulz@gmail.com>
2014-08-09 15:20:59 -0700

    How was this not commited earlier

--- app/assets/javascripts/visualizations/highvis/index.js
+++ app/assets/javascripts/visualizations/highvis/index.js
@@ -1,7 +1,8 @@
 // This is a manifest file that'll specifies the order in which these files will
 // be compiled into application.js, which will include all the files listed
-// below.
+// below. This is the bare minimum load order necessary pipeline to work.
 //
+//= require ./savedVis
 //= require ./highmodifiers
 //= require ./visUtils
 //= require ./baseVis
