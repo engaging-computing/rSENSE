51181286341b5cc665bf1e2186b96150ba436089
AlanRosenthal <alan.rosenthal@gmail.com>
2014-08-14 12:44:14 -0400

    corrected typos

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,33 +1,26 @@
 class TestingController < ApplicationController
-  # GET /testing    
+  # GET /testing
   def index
-    json_issues = JSON.parse HTTParty.get('https://api.github.com/repos/iSENSEDev/rSENSE/issues?labels=In+Testing', :headers => {"User-Agent" => "rSENSE"}).response.body
-    @issues = Array.new()
-    json_issues.each do |json_issue|
-      @issues.push(Hash["number",json_issue["number"],"html_url",json_issue["html_url"],"title",json_issue["title"]])
+    github_response = HTTParty.get('https://api.github.com/repos/iSENSEDev/rSENSE/issues?labels=In+Testing', headers: { 'User-Agent' => 'rSENSE' }).response
+    json_issues = JSON.parse github_response.body
+    @issues = []
+    if github_response.code == '200'
+      json_issues.each do |json_issue|
+        @issues.push(Hash['number', json_issue['number'], 'html_url', json_issue['html_url'], 'title', json_issue['title']])
+      end
     end
-    @issues.inspect() 
   end
-  
+
   # POST /testing/review
   def review
     @issues = params[:issues]
     @userinfo = params[:userinfo]
+    puts params.class
   end
 
   # POST /testing/publish
   def publish
-    issues = params[:issues]
-    github_auth_token = params[:userinfo][:auth_token]
-    
-    issues.each do |issue|
-        api_url = "https://api.github.com/repos/isenseDev/rSENSE/issues/#{issue[0]}/comments"
-        response = HTTParty.post(api_url,:headers => {"User-Agent" => "rSENSE","Authorization" => "token #{github_auth_token}"},:body => { "body" => issue[1][:message] }.to_json).response
-        puts response.code
-        puts response.body
-        if response.code == 201
-            puts "Issue #{issue[0]} Successfully Posted to GitHub"
-        end
-    end 
+    @issues = params[:issues]
+    @github_auth_token = params[:userinfo][:auth_token]
   end
 end
