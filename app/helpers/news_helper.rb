d14c1f1d38ce3adc6377e211c266d21aa0f5a884
James Dalphond <james.dalphond@gmail.com>
2014-08-19 13:42:20 -0400

    Increasing test coverage

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,9 +1,2 @@
 module NewsHelper
-  def news_edit_menu_helper(make_link = false)
-    render 'shared/edit_menu', type: 'news', typeName: 'News', obj: @news, make_link: make_link, escape_link: '/news'
-  end
-
-  def news_edit_helper(field, can_edit = false, make_link = true, url = '#')
-    render 'shared/edit_info', type: 'news', field: field, value: @news[field], row_id: @news.id, can_edit: can_edit, make_link: make_link, href: url
-  end
 end
