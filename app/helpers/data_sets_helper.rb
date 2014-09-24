3ceaab4d37c1aabcf7eaf922245c354bd43d11a9
Anthony Salani <asalani93@gmail.com>
2014-09-24 12:08:43 -0400

    adds almost-working slickgrid table

--- app/helpers/data_sets_helper.rb
+++ app/helpers/data_sets_helper.rb
@@ -3,4 +3,68 @@ module DataSetsHelper
     render 'shared/edit_info', type: 'data_set', field: field, value: @data_set[field],
       row_id: @data_set.id, can_edit: can_edit, make_link: make_link, href: url
   end
+
+  def format_slickgrid(fields, data_set)
+    cols, data = [fields, data_set.data]
+    cols, data = format_slickgrid_pre  cols, data
+    cols, data = format_slickgrid_json cols, data
+    [cols.to_json, data.to_json]
+  end
+
+  def format_slickgrid_pre(cols, data)
+    cols = cols.map do |x|
+      {field_type: x.field_type,
+        id: "#{x.id}",
+        name: x.name,
+        restrictions: x.restrictions,
+        units: x.unit}
+    end
+
+    coords = cols.select {|x| x[:field_type] == 4 or x[:field_type] == 5}
+
+    if coords.empty?
+      [cols, data]
+    else
+      lat = coords.select {|x| x[:field_type] == 4}[0]
+      lon = coords.select {|x| x[:field_type] == 5}[0]
+
+      # merge longitude column into latitude column
+      cols.select! {|x| x[:id] != lat[:id] and x[:id] != lon[:id]}
+      cols << {field_type: 4,
+        id: "#{lat[:id]}-#{lon[:id]}",
+        name: "#{lat[:name]} & #{lon[:name]}",
+        restrictions: '',
+        units: ''}
+
+      # copy longitude data into latitude data
+      lat_sym = lat[:id]
+      lon_sym = lon[:id]
+      data.map! do |x|
+        x["#{lat_sym}-#{lon_sym}"]  = "#{x[lat_sym]}, #{x[lon_sym]}"
+        x.delete lat_sym
+        x.delete lon_sym
+        x
+      end
+
+      [cols, data]
+    end
+  end
+
+  def format_slickgrid_json(cols, data)
+    cols_json = []
+    cols.each.with_index do |x, i|
+      editor = case x[:field_type]
+        when 1 then 'Slick.Editors.Text'
+        when 2 then 'Slick.Editors.Integer'
+        when 3 then 'Slick.Editors.Text'
+        when 4 then 'Slick.Editors.Integer'
+        when 5 then 'Slick.Editors.Integer'
+        else 'Slick.Editors.Text'
+      end
+      field = "slickgrid-#{x[:id]}"
+      cols_json << {id: "#{field}", name: "#{x[:name]}", field: "#{x[:id]}", editor: editor}
+    end
+
+    [cols_json, data]
+  end
 end
