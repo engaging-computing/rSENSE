69aef13c56ccd2440cb2509b48ce3618aa1bc96e
Jake Kinsman <Jacob_Kinsman@student.uml.edu>
2014-10-02 13:52:50 -0400

    Create media objects from base64 data in Summernote throughout the site

---
+++ db/migrate/20141002174814_save_records.rb
@@ -0,0 +1,20 @@
+class SaveRecords < ActiveRecord::Migration
+  def change
+    Project.all.each do |p|
+      p.save!
+    end
+    User.all.each do |u|
+      u.save!
+    end
+    Tutorial.all.each do |t|
+      t.save!
+    end
+    News.all.each do |n|
+      n.save!
+    end
+    Visualization.all.each do |v|
+      v.save!
+    end
+
+  end
+end
