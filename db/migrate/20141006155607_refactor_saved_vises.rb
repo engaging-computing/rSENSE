c4b3017321b1a2d6a49456c4ef0d6b4783ce9aa2
Jeremy Poulin <jaypoulz@gmail.com>
2014-10-08 10:11:00 -0400

    Wrapping up the migration

--- db/migrate/20141006155607_refactor_saved_vises.rb
+++ db/migrate/20141006155607_refactor_saved_vises.rb
@@ -9,38 +9,64 @@ class RefactorSavedVises < ActiveRecord::Migration
     old_obj.delete(key)
   end
 
-  def rupdate(old_key, new_key, vis)
-    update(new_key, old_key, vis)
+  def rupdate(old_key, new_key, obj)
+    update(new_key, old_key, obj)
   end
 
   def rmove_to_obj(key, old_obj, new_obj)
     move_to_obj(key, new_obj, old_obj)
   end
 
-  def refactor_names(obj, func)
-    func('selectedGroup', 'groupById', obj)
-    func('map', 'Map', obj)
-    func('timeline', 'Timeline', obj)
-    func('scatter', 'Scatter', obj)
-    func('bar', 'Bar', obj)
-    func('histogram', 'Histogram', obj)
-    func('pie', 'Pie', obj)
-    func('table', 'Table', obj)
-    func('summary', 'Summary', obj)
-    func('photos', 'Photos', obj)
+  def refactor_names(obj)
+    # Only for summary and there are no summary savedVises
+    obj.delete('selectedGroup')
+
+    update('map', 'Map', obj)
+    update('timeline', 'Timeline', obj)
+    update('scatter', 'Scatter', obj)
+    update('bar', 'Bar', obj)
+    update('histogram', 'Histogram', obj)
+    update('pie', 'Pie', obj)
+    update('table', 'Table', obj)
+    update('summary', 'Summary', obj)
+    update('photos', 'Photos', obj)
   end
 
-  def move_vars(globals, data, func)
-    func('groupSelection', globals, data)
+  def undo_refactor_names(obj)
+    rupdate('map', 'Map', obj)
+    rupdate('timeline', 'Timeline', obj)
+    rupdate('scatter', 'Scatter', obj)
+    rupdate('bar', 'Bar', obj)
+    rupdate('histogram', 'Histogram', obj)
+    rupdate('pie', 'Pie', obj)
+    rupdate('table', 'Table', obj)
+    rupdate('summary', 'Summary', obj)
+    rupdate('photos', 'Photos', obj)
   end
 
-  def extract_globals(key, globals, temp, func)
-    func(key, globals, temp)
+  def move_vars(globals, data)
+    move_to_obj('groupSelection', globals, data)
+    move_to_obj('groupingFieldIndex', data, globals)
+    update('groupingFieldIndex', 'groupById', globals)
   end
 
-  vises = ['Map', 'Timeline', 'Scatter', 'Bar', 'Histogram', 'Pie', 'Table', 'Summary', 'Photos']
+  def undo_move_vars(globals, data)
+    rmove_to_obj('groupSelection', globals, data)
+    rupdate('groupingFieldIndex', 'groupById', globals)
+    rmove_to_obj('groupingFieldIndex', data, globals)
+  end
+
+  def extract_globals(key, globals, temp)
+    move_to_obj(key, globals, temp)
+  end
+
+  def undo_extract_globals(key, globals, temp)
+    rmove_to_obj(key, globals, temp)
+  end
 
   def up
+    vises = ['Map', 'Timeline', 'Scatter', 'Bar', 'Histogram', 'Pie', 'Table', 'Summary', 'Photos']
+
     say 'Refactoring saved visualizations'
     # Extract the data/globals -> JSON.parse(Project.find(#).var)
     Visualization.find_each do | v |
@@ -48,15 +74,17 @@ class RefactorSavedVises < ActiveRecord::Migration
       data = JSON.parse(v.data)
 
       # Updating variable names
-      refactor_names(globals, update)
+      refactor_names(globals)
 
       # Moving group selection to globals
-      move_vars(globals, data, move_to_obj)
+      move_vars(globals, data)
 
       # Pull out anything that's not in vises into a globals object
       temp = {}
-      for key in globals.keys when !vises.include?(key)
-        extract_globals(key, globals, temp, move_to_obj)
+      for key in globals.keys
+        if !vises.include?(key)
+          extract_globals(key, globals, temp)
+        end
       end
       globals['globals'] = temp
 
@@ -71,17 +99,17 @@ class RefactorSavedVises < ActiveRecord::Migration
     say 'Restoring saved visualization format'
     # Extract the data/globals -> JSON.parse(Project.find(#).var)
     Visualization.find_each do | v |
-      globals = JSON.parse(proj.globals)
-      data = JSON.parse(proj.data)
+      globals = JSON.parse(v.globals)
+      data = JSON.parse(v.data)
 
       # Revert back to the previous configurations
       temp = globals['globals']
       globals.delete('globals')
       for key in temp.keys
-        extract_globals(key, globals, temp, rmove_to_obj)
+        undo_extract_globals(key, globals, temp)
       end
-      move_vars(globals, data, rmove_to_obj)
-      refactor_names(globals, rupdate)
+      undo_move_vars(globals, data)
+      undo_refactor_names(globals)
 
       # Update the globals
       v.globals = JSON.dump(globals)
