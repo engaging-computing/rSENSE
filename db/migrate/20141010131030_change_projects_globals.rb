c490c82e8cec79601bd98641d6b8788854b84ce2
Jeremy Poulin <jaypoulz@gmail.com>
2014-10-10 09:32:03 -0400

    Rubocop... really?

--- db/migrate/20141010131030_change_projects_globals.rb
+++ db/migrate/20141010131030_change_projects_globals.rb
@@ -2,7 +2,7 @@ class ChangeProjectsGlobals < ActiveRecord::Migration
   def change
     reversible do | dir |
       change_table :projects do |p|
-        dir.up   { p.change :globals, :text, :limit => nil }
+        dir.up   { p.change :globals, :text, limit: nil }
         dir.down { p.change :globals, :string }
       end
     end
