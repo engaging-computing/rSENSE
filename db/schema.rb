eade7c242638d381a7df581957a3c341b6763943
James Dalphond <james.dalphond@gmail.com>
2014-12-04 13:34:52 -0500

    updating old branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015152508) do
+ActiveRecord::Schema.define(version: 20141010131030) do
 
   create_table "contrib_keys", force: true do |t|
     t.string   "name",       null: false
