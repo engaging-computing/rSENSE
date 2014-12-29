ad80a1973ce88a043e2acf970899d20deb9273c9
Jake Kinsman <Jacob_Kinsman@student.uml.edu>
2014-12-29 10:27:42 -0500

    remove track changes

--- lib/simplecov_rsense.rb
+++ lib/simplecov_rsense.rb
@@ -1,9 +1,5 @@
 require 'simplecov'
 SimpleCov.profiles.define 'rsense' do
   load_profile 'rails'
-<<<<<<< HEAD
   SimpleCov.minimum_coverage 91
-=======
-  SimpleCov.minimum_coverage 87
->>>>>>> 8c39c61ac91acda03da5b287889e4ac88106291d
 end
