0554913e029e0339c626c132d79139ee69044e6f
stowellm <StowellMichael@gmail.com>
2015-01-08 14:00:29 -0500

    No more long test names that offset the beautiful row of PASSes

--- test/functional/data_sets_controller_test.rb
+++ test/functional/data_sets_controller_test.rb
@@ -9,19 +9,19 @@ class DataSetsControllerTest < ActionController::TestCase
     @crunch = users(:crunch)
   end
 
-  test 'should redirect to viz for show data set' do
+  test 'redirect to viz for show data set' do
     get :show,  id: @tgd.id
     assert_response :redirect
   end
 
-  test 'should get dataset data' do
+  test 'get dataset data' do
     get :show, { id: @tgd.id, format: 'json', recur: 'true' },  user_id: @kate
     assert_response :success
     ds = JSON.parse(@response.body)
     assert_equal 3, ds['data'][0].keys.length, 'Actually got records in data'
   end
 
-  test 'should create data_set' do
+  test 'create data_set' do
     assert_difference('DataSet.count') do
       post :create, { data_set: { project_id: @data_set.project_id,
         title: "#{@data_set.title}#{Time.now}", user_id: @data_set.user_id } },  user_id: @kate
@@ -40,7 +40,7 @@ class DataSetsControllerTest < ActionController::TestCase
     assert flash[:alert] =~ /locked/, 'Project is locked'
   end
 
-  test 'should create data_set with contrib key' do
+  test 'create data_set with contrib key' do
     assert_difference('DataSet.count') do
       post :create, { data_set: { project_id: @tgd.project_id,
         title: "#{@tgd.title}#{Time.now}" } },
@@ -50,7 +50,7 @@ class DataSetsControllerTest < ActionController::TestCase
     assert_redirected_to data_set_path(assigns(:data_set))
   end
 
-  test 'should create data_set with contrib key when not logged in' do
+  test 'create data_set with contrib key when not logged in' do
     assert_difference('DataSet.count') do
       post :create, { data_set: { project_id: @tgd.project_id,
         title: "#{@tgd.title}#{Time.now}" } },  contrib_access: @tgd.project.id
@@ -59,7 +59,7 @@ class DataSetsControllerTest < ActionController::TestCase
     assert_redirected_to data_set_path(assigns(:data_set))
   end
 
-  test 'should create data_set and get JSON response' do
+  test 'create data_set and get JSON response' do
     title = "#{@data_set.title}#{Time.now}"
     assert_difference('DataSet.count') do
       post :create, { format: 'json', data_set: {
@@ -73,19 +73,19 @@ class DataSetsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'should show data_set edit page' do
+  test 'show data_set edit page' do
     get :edit, { id: @tgd.id },  user_id: @kate
     assert @response.body =~ /Thanksgiving/, 'Response has data_set title'
     assert_response :success
   end
 
-  test 'should update data_set' do
+  test 'update data_set' do
     put :update, { id: @data_set, data_set: { project_id: @data_set.project_id,
       title: @data_set.title, user_id: @data_set.user_id } },  user_id: @kate
     assert_redirected_to data_set_path(assigns(:data_set))
   end
 
-  test 'should destroy data_set' do
+  test 'destroy data_set' do
     assert_difference('DataSet.count', -1) do
       delete :destroy, { id: @data_set },  user_id: @kate
     end
@@ -114,54 +114,54 @@ class DataSetsControllerTest < ActionController::TestCase
     assert flash[:alert] =~ /locked/, 'Project is locked'
   end
 
-  test 'should get manual entry page for locked project with key' do
+  test 'get manual entry page for locked project with key' do
     get :manualEntry, { id: @proj.id },  user_id: @crunch, contrib_access: @proj.id
     assert_response :success
   end
 
-  test 'should get manual entry page' do
+  test 'get manual entry page' do
     get :manualEntry, { id: @proj.id },  user_id: @kate
     assert_response :success
   end
 
-  test 'should export data' do
+  test 'export data' do
     get :export, { id: @proj.id, datasets: @tgd.id.to_s },  user_id: @kate
     assert(@response['Content-Type'] == 'file/zip')
   end
 
-  test 'should get data as csv string' do
+  test 'get data as csv string' do
     get :export_concatenated, { id: @proj.id, datasets: @tgd.id.to_s },  user_id: @kate
     assert response.body.include?('cookie,cake,pie'), 'Response was not correct string'
     assert_response :success
   end
 
-  test 'should upload CSV' do
+  test 'upload CSV' do
     csv_path = Rails.root.join('test', 'CSVs', 'dinner.csv')
     file = Rack::Test::UploadedFile.new(csv_path, 'text/csv')
     post :dataFileUpload, { pid: @proj.id, file: file },  user_id: @kate
     assert_response :success
   end
 
-  test 'should upload through jsonDataUpload' do
+  test 'upload through jsonDataUpload' do
     post :jsonDataUpload, { format: 'json', id: @proj.id, title: 'JSON Upload',
       data: { '20' => ['1', '2', '3'], '21' => ['4', '5', '6'], '22' => ['14', '13', '12'] } },  user_id: @kate
     assert_response :success
     @new_dataset_id = JSON.parse(response.body)['id']
   end
 
-  test 'should fail to upload through jsonDataUpload with empty dataset' do
+  test 'should fail jsonDataUpload with empty dataset' do
     post :jsonDataUpload, { format: 'json', id: @proj.id, title: 'JSON Upload',
       data: {} },  user_id: @kate
     assert_response 422
   end
 
-  test 'should not upload through jsonDataUpload for locked project' do
+  test 'should not jsonDataUpload for locked project' do
     post :jsonDataUpload, { format: 'json', id: @proj.id, title: 'JSON Upload',
       data: { '20' => ['1', '2', '3'], '21' => ['4', '5', '6'], '22' => ['14', '13', '12'] } },  user_id: @crunch
     assert_response 401
   end
 
-  test 'should upload through jsonDataUpload for locked project with key' do
+  test 'jsonDataUpload locked project with key' do
     post :jsonDataUpload, { format: 'json', id: @proj.id, title: 'JSON Upload',
       data: { '20' => ['1', '2', '3'], '21' => ['4', '5', '6'], '22' => ['14', '13', '12'] } },
       user_id: @crunch, contrib_access: @proj.id
@@ -169,7 +169,7 @@ class DataSetsControllerTest < ActionController::TestCase
     @new_dataset_id = JSON.parse(response.body)['id']
   end
 
-  test 'should upload through jsonDataUpload for locked project with key while not logged in' do
+  test 'jsonDataUpload locked project with key not logged in' do
     post :jsonDataUpload, { format: 'json', id: @proj.id, title: 'JSON Upload',
       data: { '20' => ['1', '2', '3'], '21' => ['4', '5', '6'], '22' => ['14', '13', '12'] } },
        contrib_access: @proj.id,
@@ -178,7 +178,7 @@ class DataSetsControllerTest < ActionController::TestCase
     @new_dataset_id = JSON.parse(response.body)['id']
   end
 
-  test 'should edit data set' do
+  test 'edit data set' do
     # Get original data
     get :show, { id: @tgd.id, format: 'json', recur: 'true' },  user_id: @kate
     assert_response :success
