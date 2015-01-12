70b752e7aee8a6ebf332571b8d5c1e40c2806bf8
bobby <bobbydonald25@gmail.com>
2015-01-12 15:52:42 -0500

    fixes rubocop issues

--- test/integration/api_v1_test.rb
+++ test/integration/api_v1_test.rb
@@ -585,7 +585,7 @@ class ApiV1Test < ActionDispatch::IntegrationTest
           }
     assert_response :unauthorized
   end
-  
+
   test 'add a key to a project' do
     post '/api/v1/projects',
 
@@ -594,15 +594,15 @@ class ApiV1Test < ActionDispatch::IntegrationTest
 
     assert_response :success
     id = parse(response)['id']
-    
+
     post '/api/v1/projects/3/key',
         email: 'kcarcia@cs.uml.edu',
         password: '12345',
         contrib_key:
           {
-             'name' => "key_name",
-             'project_id' => id,
-             'key' => 'key'
+            'name' => 'key_name',
+            'project_id' => id,
+            'key' => 'key'
           }
     assert_response :created
   end
