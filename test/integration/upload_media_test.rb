c2c710f53a68a3ce23785f88f9803ea908ac8cac
Jake Kinsman <Jacob_Kinsman@student.uml.edu>
2015-01-13 11:53:37 -0500

    Now checks for exactly one occurrence of nerdboy.  whoops!

--- test/integration/upload_media_test.rb
+++ test/integration/upload_media_test.rb
@@ -88,18 +88,16 @@ class UploadMediaTest < ActionDispatch::IntegrationTest
     find('.upload_media form').attach_file('upload', html_path)
     assert page.has_no_content?('test.html'), 'File should be in list'
     assert page.has_content?('Sorry, html is not a supported file type.'), 'Unsupported file type failed.'
-    nerdboy_found = false
+    nerdboy_found = 0
     @project = Project.find(proj_id)
     @project.media_objects.length.times do |i|
       visit "/projects/#{proj_id}"
       all('.media_edit')[i].click
-      if page.html.include? 'nerdboy.jpg' and nerdboyFound == false
-        nerdboy_found = true
-      elsif page.html.include? 'nerdboy.jpg' and nerdboyFound == true
-        nerdboy_found = false
+      if page.html.include? 'nerdboy.jpg'
+        nerdboy_found += 1
       end
       assert page.html.include? "data-page-name=\"media_objects/show\""
     end
-    assert nerdboyFound, 'Nerdboy was not displayed exactly once on the media objects view.'
+    assert nerdboy_found == 1, 'Nerdboy was not displayed exactly once on the media objects view.'
   end
 end
