b2b65d941e55cefc5678d4f314338de1a7883d36
Anthony Salani <asalani93@gmail.com>
2015-01-13 13:13:37 -0500

    more tests

--- test/helpers/data_sets_helper_test.rb
+++ test/helpers/data_sets_helper_test.rb
@@ -3,9 +3,14 @@ require 'test_helper'
 class DataSetsHelperTest < ActionView::TestCase
   include DataSetsHelper
 
-  test 'slickgrid data formatting - merge latitude and longitude columns' do
-    proj = Project.find_by_name 'slickgrid_project'
-    cols, data = format_slickgrid_merge proj.fields, proj.data_sets[0].data
+  def assert_similar_arrays(a, b)
+    assert (a - b).length + (b - a).length == 0, "Arrays #{a} and #{b} do not have the same contents"
+  end
+
+  test 'slickgrid data formatting - merge lat and lon' do
+    project = Project.find_by_name 'slickgrid_project'
+    dataset = DataSet.find_by_name 'Slickgrid Dataset 1'
+    cols, data = format_slickgrid_merge project.fields, dataset.data
 
     rescols = [
       {field_type: 1, id: '103', name: 'timestamp_field', restrictions: '""', units: ''},
@@ -14,24 +19,46 @@ class DataSetsHelperTest < ActionView::TestCase
       {field_type: 3, id: '101', name: 'restrictions_field', restrictions: ['A', 'B', 'C'], units: ''},
       {field_type: 4, id: '104-105', name: 'latitude_field & longitude_field', restrictions: '""', units: ''}
     ]
-    assert cols - rescols == []
+    assert_similar_arrays cols, rescols
 
     resdata = [
       {'100' => 'A', '101' => 'A', '102' => '1', '103' => '11', '104-105' => '0, 0'},
       {'100' => 'B', '101' => 'B', '102' => '2', '103' => '22', '104-105' => '1, 1'},
       {'100' => 'C', '101' => 'C', '102' => '3', '103' => '33', '104-105' => '2, 2'}
     ]
-    assert data - resdata == []
+    assert_similar_arrays data, resdata
   end
 
-  test 'slickgrid data formatting - skip latitude & longitude merging' do 
-    proj = Project.find_by_name 'slickgrid_project_no_latlon'
-    cols, data = format_slickgrid_merge proj.fields, proj.data_sets[0].data
+  test 'slickgrid data formatting - skip lat & lon merge' do 
+    project = Project.find_by_name 'slickgrid_project_no_latlon'
+    dataset = DataSet.find_by_name 'Slickgrid Dataset 2'
+    cols, data = format_slickgrid_merge project.fields, dataset.data
 
     rescols = [{field_type: 3, id: '106', name: 'something', restrictions:'""', units: ''}]
-    assert cols - rescols == []
+    assert_similar_arrays cols, rescols
 
     resdata = [{'106' => 'A'}, {'106' => 'B'}, {'106' => 'C'}]
-    assert data - resdata == []
+    assert_similar_arrays data, resdata
+  end
+
+  test 'slickgrid data population w/ data' do
+    project = Project.find_by_name 'slickgrid_project_no_latlon'
+    dataset = DataSet.find_by_name 'Slickgrid Dataset 2'
+    cols, data = format_slickgrid_merge project.fields, dataset.data
+    cols_pop, data_pop = format_slickgrid_populate cols, data
+
+    assert_similar_arrays cols, cols_pop
+    assert_similar_arrays data, data_pop
+  end
+
+  test 'slickgrid data population w/o data' do
+    project = Project.find_by_name 'slickgrid_project_no_latlon'
+    dataset = DataSet.find_by_name 'Slickgrid Dataset 3'
+    cols, data = format_slickgrid_merge project.fields, dataset.data
+    cols_pop, data = format_slickgrid_populate cols, data
+    data_pop = [{id: 0, '106' => ''}]
+
+    assert_similar_arrays cols, cols_pop
+    assert_similar_arrays data, data_pop
   end
 end
