d6d9170a09fce37fc2bb93aa1156e2867bda4f75
bobby <bobbydonald25@gmail.com>
2015-01-21 15:14:22 -0500

    key -> add_key

--- test/integration/api_v1_test.rb
+++ test/integration/api_v1_test.rb
@@ -595,7 +595,7 @@ class ApiV1Test < ActionDispatch::IntegrationTest
     assert_response :success
     id = parse(response)['id']
 
-    post '/api/v1/projects/3/key',
+    post '/api/v1/projects/3/add_key',
         email: 'kcarcia@cs.uml.edu',
         password: '12345',
         contrib_key:
@@ -605,6 +605,27 @@ class ApiV1Test < ActionDispatch::IntegrationTest
             'key' => 'key'
           }
     assert_response :created
+  end
+
+  test 'add a key to a project unauthorized' do
+    post '/api/v1/projects',
+
+    email: 'kcarcia@cs.uml.edu',
+    password: '12345'
+
+    assert_response :success
+    id = parse(response)['id']
+
+    post '/api/v1/projects/3/add_key',
+        email: 'kcarcia@cs.uml.edu',
+        password: '12345',
+        contrib_key:
+          {
+            'name' => 'key_name',
+            'project_id' => id,
+            'key' => 'key'
+          }
+    assert_response :unauthorized
   end
 
   private
