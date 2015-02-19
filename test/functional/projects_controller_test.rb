3520e86c736dfad37e87e1aa17392de4df4c29ab
Kaitlyn Carcia <kate.carcia@gmail.com>
2015-02-19 13:49:30 -0500

    RUBOCOP -___-

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -216,8 +216,6 @@ class ProjectsControllerTest < ActionController::TestCase
     parameters['user_id'] = @kate.id
 
     # Adds a Latitude and Longitude field to determine where the dinner was eaten!
-
-    
     post :save_fields, { id: @dessert.id,  field: { id: 23, project_id: @dessert.id, field_type: 4, name: 'Location of Foods' },
                         '20_name' => parameters['20_name'], '20_unit' => parameters['20_unit'], '21_name' => parameters['21_name'],
                         '21_unit' => parameters['21_unit'], '22_name' => parameters['22_name'], '22_unit' => parameters['22_unit'],
@@ -243,7 +241,7 @@ class ProjectsControllerTest < ActionController::TestCase
                       '21_unit' => parameters['21_unit'], '22_name' => parameters['22_name'], '22_unit' => parameters['22_unit'],
                       "#{new_lat_field_id}_name" => @project.fields[num_fields - 2].name, "#{new_lat_field_id}_unit" => @project.fields[num_fields - 2].unit,
                       "#{new_long_field_id}_name" => @project.fields[num_fields - 1].name, "#{new_long_field_id}_unit" => @project.fields[num_fields - 1].unit,
-                      '20_restrictions' => 'a,b,c'}, user_id: @kate.id
+                      '20_restrictions' => 'a,b,c' }, user_id: @kate.id
     assert_redirected_to "/projects/#{@dessert.id}"
 
     # Tests empty value in restrictions hash
