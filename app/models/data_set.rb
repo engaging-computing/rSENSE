eea5b5cd8409ada7bed6f82cbcdc03d92d3d8f94
Bobby <bobbydonald25@gmail.com>
2015-02-24 12:24:17 -0500

    username and key to dataset hash

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -44,6 +44,8 @@ class DataSet < ActiveRecord::Base
     h = {
       id: id,
       name: title,
+      ownersUserId: user.id,
+      contribKey: key,
       url: UrlGenerator.new.data_set_url(self),
       path: UrlGenerator.new.data_set_path(self),
       createdAt: created_at.strftime('%B %d, %Y'),
