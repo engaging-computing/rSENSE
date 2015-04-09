86936b2cb0bf8bc3e20a36164510de65416c3325
James Dalphond <james.dalphond@gmail.com>
2015-04-09 16:10:44 -0400

    Getting rid of useless ajax that only admins see. #2083 #2084

--- test/integration/tutorials_test.rb
+++ test/integration/tutorials_test.rb
@@ -13,34 +13,42 @@ class TutorialsTest < ActionDispatch::IntegrationTest
   end
 
   test 'create a tutorial' do
+
+    # Make sure a regular user cant create a tutorial
     login('kcarcia@cs.uml.edu', '12345')
     click_on 'Tutorials'
     click_on 'See All Tutorials'
     assert page.has_no_content?('Create Tutorial'), 'Non-Admin should not be able to create a tutorial'
     logout
 
+    # An Admin should be able to create a Tutorial
     login('nixon@whitehouse.gov', '12345')
     visit '/tutorials'
     assert page.has_content?('Create Tutorial'), 'Admin should be able to create a tutorial'
-
     find('#tutorial_title').set('Awesome Tutorial')
     click_on 'Create Tutorial'
-
     assert page.has_content?('Awesome Tutorial'), 'Should have ended up on tutorials show page'
 
+    # An Admin should be able to publish a tutorial.
     find('#publish_tutorial').click
+    assert page.has_content?('Tutorial was successfully updated.'), 'Tutorial was not saved.'
 
-    assert page.has_content?('Saved.'), 'Tutorial was not saved.'
-
+    # Once published anyone should be able to see the tutorial
     visit '/tutorials'
-    assert page.has_content?('Awesome Tutorial'), 'Tutorial should have been published'
+    assert page.has_content?('Awesome Tutorial'), 'All tutorials should be visible to admins'
+    logout
+    visit '/tutorials'
+    assert page.has_content?('Awesome Tutorial'), 'Published tutorial should be visible to user'
 
+    # Unpublish the tutorial
+    login('nixon@whitehouse.gov', '12345')
+    visit '/tutorials'
     click_on 'Awesome Tutorial'
-    assert page.has_content?('Publish'), 'Should have ended up on tutorials show page'
+    assert page.has_content?('Description'), 'Should have ended up on tutorials show page'
     find('#publish_tutorial').click
+    assert page.has_content?('Tutorial was successfully updated.'), 'Tutorial was not saved.'
 
-    assert page.has_content?('Saved.'), 'Tutorial was not saved.'
-
+    logout
     visit '/tutorials'
     assert page.has_no_content?('Awesome Tutorial'), 'Tutorial should have been unpublished'
   end
