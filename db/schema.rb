1c26914ad7367a4968a9fe8d32492173f9677dc8
stowellm <StowellMichael@gmail.com>
2015-04-14 15:42:14 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225125927) do
+ActiveRecord::Schema.define(version: 20150403162309) do
 
   create_table "contrib_keys", force: true do |t|
     t.string   "name",       null: false
