acd19fa52588e059fe533ce1fdba5e22e42557ba
stowellm <StowellMichael@gmail.com>
2015-04-14 14:45:13 -0400

    Migration works but needs testing

--- db/migrate/20150403162309_refactor_invisible_fields.rb
+++ db/migrate/20150403162309_refactor_invisible_fields.rb
@@ -5,11 +5,11 @@ class RefactorInvisibleFields < ActiveRecord::Migration
   # the name, unit, and type for your new hidden field.  This
   # information can all be found by looking at the 'data' JSON
   # dump of a saved visualization that includes your hidden field.
-  n = 2
-  name = "Contributors"
-  unit = "String"
-  type = 3
-  hash_name = "CONTRIBUTOR_FIELD"
+  $n = 2
+  $name = "Contributors"
+  $unit = "String"
+  $type = 3
+  $hash_name = "CONTRIBUTOR_FIELD"
 
   def up
     refactor(1)
@@ -20,9 +20,15 @@ class RefactorInvisibleFields < ActiveRecord::Migration
   end
 
   def refactor(dir)
-    say 'Adding new virtual field to projects and saved vises'
+    if (dir > 0)
+      say 'Adding new virtual field to projects and saved vises'
+    else
+      say 'Removing virtual field from projects and saved vises'
+    end
 
     Project.find_each do | p |
+      next if p.globals.nil?
+
       # projects require a globals update
       globals = JSON.parse(p.globals)
 
@@ -36,16 +42,20 @@ class RefactorInvisibleFields < ActiveRecord::Migration
 
     Visualization.find_each do | v |
       # vises require a globals and data update
-      globals = JSON.parse(v.globals)
-      data = JSON.parse(v.data)
+      globals = v.globals.nil? ? nil : JSON.parse(v.globals)
+      data = v.data.nil? ? nil : JSON.parse(v.data)
 
       # update everything field related
-      globals = refactor_globals(globals, dir)
-      data = refactor_data(data, dir)
+      unless globals.nil?
+        globals = refactor_globals(globals, dir)
+        v.globals = JSON.dump(globals)
+      end
+      unless data.nil?
+        data = refactor_data(data, dir)
+        v.data = JSON.dump(data)
+      end
 
       # save the new globals and data
-      v.globals = JSON.dump(globals)
-      v.data = JSON.dump(data)
       v.save
     end
   end
@@ -53,85 +63,113 @@ class RefactorInvisibleFields < ActiveRecord::Migration
   def refactor_globals(globals, dir)
     # global
     subglobal = globals['global']
-    if subglobal['groupById'] >= n
-      subglobal['groupById'] += dir
-    end
-    if subglobal['fieldSelection'] >= n
-      subglobal['fieldSelection'] += dir
+    unless subglobal.nil?
+      if subglobal['groupById'] >= $n
+        subglobal['groupById'] += dir
+      end
+      if subglobal['fieldSelection'] >= $n
+        subglobal['fieldSelection'] += dir
+      end
     end
 
     # map unaffected
 
     # timeline
     timeline = globals['Timeline']
-    if timeline['xAxis'] >= n
-      timeline['xAxis'] += dir
-    end
-    timeline['yAxis'].each_with_index do | y, i |
-      if y >= n
-        timeline['yAxis'][i] += dir
+    unless timeline.nil?
+      if !timeline['xAxis'].nil? && timeline['xAxis'] >= $n
+        timeline['xAxis'] += dir
       end
-    end
-    timeline['savedRegressions'].each do | regres |
-      if regres['xAxis'] >= n
-        regres['xAxis'] += dir
+      unless timeline['yAxis'].nil?
+        timeline['yAxis'].each_with_index do | y, i |
+          if y >= $n
+            timeline['yAxis'][i] += dir
+          end
+        end
       end
-      if regres['yAxis'] >= n
-        regres['yAxis'] += dir
+      unless timeline['savedRegressions'].nil?
+        timeline['savedRegressions'].each do | regres |
+          if !regres['xAxis'].nil? && regres['xAxis'] >= $n
+            regres['xAxis'] += dir
+          end
+          if !regres['yAxis'].nil? && regres['yAxis'] >= $n
+            regres['yAxis'] += dir
+          end
+        end
       end
     end
 
     # scatter
     scatter = globals['Scatter']
-    if scatter['xAxis'] >= n
-      scatter['xAxis'] += dir
-    end
-    scatter['yAxis'].each_with_index do | y, i |
-      if y >= n
-        scatter['yAxis'][i] += dir
-      end
-    end
-    scatter['savedRegressions'].each do | regres |
-      if scatter['xAxis'] >= n
+    unless scatter.nil?
+      if !scatter['xAxis'].nil? && scatter['xAxis'] >= $n
         scatter['xAxis'] += dir
       end
-      if regres['yAxis'] >= n
-        regres['yAxis'] += dir
+      unless scatter['yAxis'].nil?
+        scatter['yAxis'].each_with_index do | y, i |
+          if y >= $n
+            scatter['yAxis'][i] += dir
+          end
+        end
+      end
+      unless scatter['savedRegressions'].nil?
+        scatter['savedRegressions'].each do | regres |
+          if !regres['xAxis'].nil? && regres['xAxis'] >= $n
+            regres['xAxis'] += dir
+          end
+          if !regres['yAxis'].nil? && regres['yAxis'] >= $n
+            regres['yAxis'] += dir
+          end
+        end
       end
     end
 
     # bar
     bar = globals['Bar']
-    if bar['sortField'] >= n
-      bar['sortField'] += dir
+    unless bar.nil?
+      if !bar['sortField'].nil? && bar['sortField'] >= $n
+        bar['sortField'] += dir
+      end
     end
 
     # histogram
     histogram = globals['Histogram']
-    if histogram['displayField'] >= n
-      histogram['displayField'] += dir
+    unless histogram.nil?
+      if !histogram['displayField'].nil? && histogram['displayField'] >= $n
+        histogram['displayField'] += dir
+      end
     end
 
     # pie
     pie = globals['Pie']
-    if pie['displayField'] >= n
-      pie['displayField'] += dir
+    unless pie.nil?
+      if !pie['displayField'].nil? && pie['displayField'] >= $n
+        pie['displayField'] += dir
+      end
     end
 
     # table
     table = globals['Table']
-    table['tableFields'].each_with_index do | t, i |
-      if t >= n
-        table['tableFields'][i] += dir
+    unless table.nil? || table['tableFields'].nil?
+      table['tableFields'].each_with_index do | t, i |
+        if t >= $n
+          table['tableFields'][i] += dir
+        end
       end
+      if (dir > 0)
+        table['tableFields'].push($n)
+      else
+        table['tableFields'] -= [$n]
+      end
+      table['tableFields'].sort!
     end
-    table['tableFields'].push(n)
-    table['tableFields'].sort!
 
     # summary
     summary = globals['Summary']
-    if summary['displayField'] >= n
-      summary['displayField'] += dir
+    unless summary.nil?
+      if !summary['displayField'].nil? && summary['displayField'] >= $n
+        summary['displayField'] += dir
+      end
     end
 
     # photos unaffected
@@ -142,86 +180,101 @@ class RefactorInvisibleFields < ActiveRecord::Migration
   def refactor_data(data, dir)
     # add the new hidden field, or remove if direction is down
     fields = data['fields']
-    if (dir > 0)
-      new_field = Hash.new
-      new_field['typeID'] = type
-      new_field['unitName'] = unit
-      new_field['fieldID'] = -1
-      new_field['fieldName'] = name
-      fields.insert(n + 1, new_field)
-    else
-      fields.delete_at(n + 1)
+    unless fields.nil?
+      if (dir > 0)
+        new_field = Hash.new
+        new_field['typeID'] = $type
+        new_field['unitName'] = $unit
+        new_field['fieldID'] = -1
+        new_field['fieldName'] = $name
+        fields.insert($n + 1, new_field)
+      else
+        fields.delete_at($n + 1)
+      end
+    end
 
     # insert a blank string as the hidden field for this object;
     # doing so makes this migration modular and applicable to
     # future uses of this migration when adding more hidden fields
     dp = data['dataPoints']
-    dp.each_with_index do | d, i |
-      if (dir > 0)
-        dp[i].insert(n + 1, "")
-      else
-        dp[i].delete_at(n + 1)
+    unless dp.nil?
+      dp.each_with_index do | d, i |
+        if (dir > 0)
+          dp[i].insert($n + 1, "")
+        else
+          dp[i].delete_at($n + 1)
+        end
       end
     end
 
     # add to the data hash the new hidden field index, or remove
     # if the direction is down
     if (dir > 0)
-      data[hash_name] = n + 1
+      data[$hash_name] = $n + 1
     else
-      data.delete(hash_name)
+      data.delete($hash_name)
     end
 
     # update field arrays
     text_f = data['textFields']
-    text_f.each_width_index do | t, i |
-      if t >= n
-        text_f[i] += dir
+    unless text_f.nil?
+      text_f.each_with_index do | t, i |
+        if t >= $n
+          text_f[i] += dir
+        end
       end
+      if (dir > 0)
+        # add the new field to the array
+        text_f.push($n)
+      else
+        # remove the new field from the array
+        text_f -= [$n]
+      end
+      text_f.sort!
     end
-    if (dir > 0)
-      # add the new field to the array
-      text_f.push(n)
-    else
-      # remove the new field from the array
-      text_f -= [n]
-    end
-    text_f.sort!
 
     time_f = data['timeFields']
-    time_f.each_width_index do | t, i |
-      if t >= n
-        time_f[i] += dir
+    unless time_f.nil?
+      time_f.each_with_index do | t, i |
+        if t >= $n
+          time_f[i] += dir
+        end
       end
     end
 
     norm_f = data['normalFields']
-    norm_f.each_width_index do | t, i |
-      if t >= n
-        norm_f[i] += dir
+    unless norm_f.nil?
+      norm_f.each_with_index do | t, i |
+        if t >= $n
+          norm_f[i] += dir
+        end
       end
     end
 
     num_f = data['numericFields']
-    num_f.each_width_index do | t, i |
-      if t >= n
-        num_f[i] += dir
+    unless num_f.nil?
+      num_f.each_with_index do | t, i |
+        if t >= $n
+          num_f[i] += dir
+        end
       end
     end
 
     geo_f = data['normalFields']
-    geo_f.each_width_index do | t, i |
-      if t >= n
-        geo_f[i] += dir
+    unless geo_f.nil?
+      geo_f.each_with_index do | t, i |
+        if t >= $n
+          geo_f[i] += dir
+        end
       end
     end
 
     # add the hidden field to group selection, or remove
     # if direction is down
     if (dir > 0)
-      data['groupSelection'].push(n)
+      data['groupSelection'].push($n)
     else
-      data['groupSelection'] -= [n]
+      data['groupSelection'] -= [$n]
     end
 
     data
