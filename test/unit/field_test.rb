988d71751dce7595bd1b90b227544f0dd7d40cc5
Anthony Salani <asalani93@gmail.com>
2015-06-05 10:31:11 -0400

    test coverage at 90.14%

--- test/unit/field_test.rb
+++ test/unit/field_test.rb
@@ -28,6 +28,12 @@ class FieldTest < ActiveSupport::TestCase
     @field2.restrictions = 4
     assert !@field2.valid?, 'Result should not be valid'
   end
+  
+  test 'fail with bad array restrictions' do
+    assert @field2.valid?, 'Field should initially be valid'
+    @field2.restrictions = [:some_label, [1, 2, 3]]
+    assert !@field2.valid?, 'Result should not be valid'
+  end
 
   # Passes if field passes validation
   test 'pass with good restrictions' do
