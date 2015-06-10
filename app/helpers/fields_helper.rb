8e71f400c79642bb73d4a36cb47793c11ad0e10b
Anthony Salani <asalani93@gmail.com>
2015-06-10 09:02:27 -0400

    removes unused code in fields helper

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -1,35 +1,3 @@
 module FieldsHelper
-  def get_field_name(field)
-    case field
-    when 1
-      'Timestamp'
-    when 2
-      'Number'
-    when 3
-      'Text'
-    when 4
-      'Latitude'
-    when 5
-      'Longitude'
-    else
-      'invalid input: try get_field_type(int)'
-    end
-  end
-
-  def get_field_type(field)
-    case field
-    when 'Timestamp'
-      1
-    when 'Number'
-      2
-    when 'Text'
-      3
-    when 'Latitude'
-      4
-    when 'Longitude'
-      5
-    else
-      'invalid input: try get_field_name(string)'
-    end
-  end
+  
 end
