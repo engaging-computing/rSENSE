e3f5825b3544462e3babd72e94ca9e7b8d8cf61b
Anthony Salani <asalani93@gmail.com>
2015-06-15 12:20:38 -0400

    should fix the test

--- test/integration/rename_vis_test.rb
+++ test/integration/rename_vis_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class RenameVisTest < ActionDispatch::IntegrationTest
   include CapyHelper
+  
+  self.use_transactional_fixtures = false
 
   setup do
     Capybara.current_driver = :webkit
@@ -15,8 +17,7 @@ class RenameVisTest < ActionDispatch::IntegrationTest
   test 'create modify and delete a vis' do
     login('kcarcia@cs.uml.edu', '12345')
 
-    click_on 'Projects'
-    click_on 'Dessert is Delicious'
+    visit "/projects/#{projects(:dessert).id}"
     click_on 'Visualize'
 
     page.execute_script "$('#save-ctrls > .vis-ctrl-header').click()"
