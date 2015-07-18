656e327cf620d27e5801e998cca1f418e3fe50a4
James Dalphond <james.dalphond@gmail.com>
2015-07-18 11:47:43 -0400

    Adding tests for new api change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514161235) do
+ActiveRecord::Schema.define(version: 20150519161334) do
 
   create_table "contrib_keys", force: true do |t|
     t.string   "name",       null: false
@@ -137,6 +137,14 @@ ActiveRecord::Schema.define(version: 20150514161235) do
     t.datetime "updated_at"
   end
 
+  create_table "vislogs", force: true do |t|
+    t.text     "data"
+    t.integer  "visualization_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "visualizations", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
@@ -151,6 +159,7 @@ ActiveRecord::Schema.define(version: 20150514161235) do
     t.datetime "featured_at"
     t.text     "summary"
     t.integer  "thumb_id"
+    t.boolean  "should_log",  default: false
   end
 
 end
