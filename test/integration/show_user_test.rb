efa724010f3aaa192b267e509e7b225e18a5e720
bobby <bobbydonald25@gmail.com>
2015-08-11 13:48:41 -0400

    fix test

--- test/integration/show_user_test.rb
+++ test/integration/show_user_test.rb
@@ -33,6 +33,7 @@ class ShowUserTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Media Test'), 'View projects list'
 
     # Verify existence of and count of delete project links
+    expect(page).to have_selector('.contrib-delete-link', visible: true)
     assert page.has_css?('.contrib-delete-link'), 'Delete project should exist'
     count = page.all(:css, '.contrib-delete-link').length
 
