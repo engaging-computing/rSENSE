eaa7f7f734c64e482284a72e9185650dcd036b3b
James Dalphond <james.dalphond@gmail.com>
2015-09-24 17:23:59 -0400

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -138,14 +138,6 @@ ActiveRecord::Schema.define(version: 20150805145056) do
     t.datetime "updated_at"
   end
 
-  create_table "vislogs", force: true do |t|
-    t.text     "data"
-    t.integer  "visualization_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "visualizations", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
@@ -160,7 +152,6 @@ ActiveRecord::Schema.define(version: 20150805145056) do
     t.datetime "featured_at"
     t.text     "summary"
     t.integer  "thumb_id"
-    t.boolean  "should_log",  default: false
   end
 
 end
