c2ac33095bb1fa535c46dd8c0a26b2c10d00e5d2
pat-son <patrick.son@comcast.net>
2015-11-06 16:05:36 -0500

    Fixed issue in which the timeline vis was not disabled when it should be

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -239,23 +239,31 @@ class VisualizationsController < ApplicationController
     data_fields.push(typeID: TEXT_TYPE, unitName: 'String', fieldID: -1, fieldName: 'Data Set Name (id)')
     # create special grouping field for all datasets
     data_fields.push(typeID: TEXT_TYPE, unitName: 'String', fieldID: -1, fieldName: 'Combined Data Sets')
+    # lat and time_field are for verifying that the correct data is present for the relevant vis
     lat = ''
+    time_field = ''
     # push real fields to temp variable
     @project.fields.sort_by(&:index).each do |field|
       data_fields.push(typeID: field.field_type, unitName: field.unit, fieldID: field.id, fieldName: field.name)
+      #check if location and timestamp fields exist
       lat = field.id.to_s if field.field_type == 4
+      time_field = field.id.to_s if field.field_type == 1
     end
 
     has_pics = false
     has_loc_data = false
+    has_time_data = false
     # create/push metadata for datasets
     i = 0
+    time_count = 0
     @datasets.each do |dataset|
       photos = dataset.media_objects.to_a.keep_if { |mo| mo.media_type == 'image' }.map { |mo| mo.to_hash(true) }
       has_pics = true if photos.size > 0
       metadata[i] = { name: dataset.title, user_id: dataset.user_id, dataset_id: dataset.id, timecreated: dataset.created_at, timemodified: dataset.updated_at, photos: photos }
       dataset.data.each_with_index do |row, index|
+        # check if location and/or timestamp data exists
         has_loc_data = true if has_loc_data == false and lat != '' and row.has_key?(lat) and row[lat] != ''
+        time_count += 1 if time_count < 3 and time_field != '' and row.has_key?(time_field) and row[time_field] != ''
         unless row.class == Hash
           logger.info 'Bad row in JSON data:'
           logger.info row.inspect
@@ -276,6 +284,9 @@ class VisualizationsController < ApplicationController
       i += 1
     end
 
+    # Timeline needs at least 3 datapoints
+    has_time_data = true if time_count > 2
+
     # Count the number of each type of field
     field_count = [0, 0, 0, 0, 0, 0]
     @project.fields.each do |field|
@@ -290,7 +301,7 @@ class VisualizationsController < ApplicationController
     end
 
     if field_count[TIME_TYPE] > 0 and field_count[NUMBER_TYPE] > 0 and
-       format_data.count > 2
+       format_data.count > 2 and has_time_data
       rel_vis.push 'Timeline'
     end
 
---
+++ test/integration/timeline_no_data_disable_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+require_relative 'base_integration_test'
+
+class MapNoDataDisableTest < IntegrationTest
+  self.use_transactional_fixtures = false
+
+  setup do
+    @project_id = projects(:timeline_vis_disable).id
+    @dataset_1_id = data_sets(:timeline_vis_disable_dset_with_time).id
+    @dataset_2_id = data_sets(:timeline_vis_disable_dset_no_time).id
+    @dataset_3_id = data_sets(:timeline_vis_disable_dset_some_time).id
+  end
+
+  test 'timeline vis enabled: dset 1, 3 timestamps' do
+    visit "/projects/#{@project_id}/data_sets/#{@dataset_1_id}"
+    click_on "Table"
+    assert page.has_no_css?('#vis-tab-timeline.strikethrough'), 'timeline vis should be enabled'
+  end
+
+  test 'timeline vis disabled: dset 2, 0 timestamps' do
+    visit "/projects/#{@project_id}/data_sets/#{@dataset_2_id}"
+    assert page.has_css?('#vis-tab-timeline.strikethrough'), 'timeline vis should be disabled'
+  end
+
+  test 'timeline vis disabled: dset 3, 2 timestamps' do
+    visit "/projects/#{@project_id}/data_sets/#{@dataset_3_id}"
+    assert page.has_css?('#vis-tab-timeline.strikethrough'), 'timeline vis should be disabled'
+  end
+
+  test 'timeline vis enabled: all dsets' do
+    visit "/projects/#{@project_id}/data_sets/#{@dataset_1_id},#{@dataset_2_id},#{@dataset_3_id}"
+    assert page.has_no_css?('#vis-tab-timeline.strikethrough'), 'timeline vis should be enabled'
+  end
+end
