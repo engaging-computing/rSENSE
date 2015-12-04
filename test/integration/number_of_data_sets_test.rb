30c5a2aa6a4ee72d8c75d1751c40b79d1edbf617
bobby <bobbydonald25@gmail.com>
2015-12-03 21:36:36 -0500

    fix test

--- test/integration/number_of_data_sets_test.rb
+++ test/integration/number_of_data_sets_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
+require 'capybara-screenshot'
 require_relative 'base_integration_test'
 
 class UploadDataTest < IntegrationTest
   self.use_transactional_fixtures = false
 
-  setup do
-    @project = projects(:lots_of_data_sets)
-  end
-
   test 'correct number of data sets' do
+    @project = projects(:lots_of_data_sets)
+
     visit project_path(@project)
+    screenshot_and_open_image
     assert page.has_content?('Lots of Data Sets'), 'Not on project page.'
 
     first('.mdl-checkbox ').click
