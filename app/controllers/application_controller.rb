f561698ebef8146135d21bb72b2a8b0908e5d7b0
james.dalphond@gmail.com <james.dalphond@gmail.com>
2015-12-14 15:31:30 -0500

    Adding tests for registration and logging in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(_resource_or_scope)
-    '/'
+    logger.error "---#{request.referrer}--"
+    logger.error "-----------#{params[:controller]}-------------"
+    if request.referrer.include? '/users/'
+      '/'
+    else
+      request.referrer
+    end
   end
 
   def after_sign_in_path_for(_resource_or_scope)
--- test/integration/register_test.rb
+++ test/integration/register_test.rb
@@ -20,5 +20,35 @@ class RegisterTest < IntegrationTest
     login('msherman@cs.uml.edu', 'pietimes')
 
     assert find('.navbar').has_content?('News'), 'Can log in with new user'
+
+    logout
+  end
+
+  test 'fail to create user for taken email' do
+    visit '/'
+    find('.navbar').click_on('Register')
+    assert page.has_content?('Register for iSENSE')
+    fill_in 'user_name',       with: 'Richard Nixon'
+    fill_in 'user_email',      with: 'nixon@whitehouse.gov'
+    fill_in 'user_password',   with: 'notacrook'
+    fill_in 'user_password_confirmation',
+                               with: 'notacrook'
+    click_on 'Create User'
+
+    assert page.has_content?('Email has already been taken'), 'Created duplicate user'
+  end
+
+  test 'fail to create user for password length' do
+    visit '/'
+    find('.navbar').click_on('Register')
+    assert page.has_content?('Register for iSENSE')
+    fill_in 'user_name',       with: 'Richard Nixon'
+    fill_in 'user_email',      with: 'nixon@whitehouse.gov'
+    fill_in 'user_password',   with: 'short'
+    fill_in 'user_password_confirmation',
+                               with: 'short'
+    click_on 'Create User'
+
+    assert page.has_content?('Password is too short (minimum is 8 characters)'), 'Created account with too short of a password'
   end
 end
