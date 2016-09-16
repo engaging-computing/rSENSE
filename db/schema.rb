11c363371a95e761dc0a88ba25eb13334f92026b
Victoria Albanese <Victoria_Albanese@student.uml.edu>
2016-09-16 12:46:08 -0400

    fixed bug, before pushing need to fix deleting tutorials bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160425161529) do
-=======
-ActiveRecord::Schema.define(version: 20160424012336) do
->>>>>>> upstream/master
 
   create_table "contrib_keys", force: true do |t|
     t.string   "name",       null: false
