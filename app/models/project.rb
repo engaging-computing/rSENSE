d9f476faffb7c62c6788161a2f3230719e7a7c18
Chike Abuah <abuahchu@gmail.com>
2017-01-28 20:16:52 -0500

    add tag search to search box

--- app/models/project.rb
+++ app/models/project.rb
@@ -54,7 +54,10 @@ class Project < ActiveRecord::Base
     regex = /^[0-9]+$/
     search_query = Project.joins(
       'LEFT OUTER JOIN "likes" ON "likes"."project_id" = "projects"."id"
-      LEFT OUTER JOIN "view_counts" ON "view_counts"."project_id" = "projects"."id"'
+      LEFT OUTER JOIN "view_counts" ON "view_counts"."project_id" = "projects"."id"
+      INNER JOIN "taggings" ON "taggings"."project_id" = "projects"."id"
+      INNER JOIN "tags" ON "taggings"."tag_id" = "tags"."id"
+      '
       ).select(
       'projects.*, count(likes.id) as like_count, view_counts.count as views'
       ).group(
@@ -66,7 +69,8 @@ class Project < ActiveRecord::Base
     elsif search
       search_query.where(
      '(lower(projects.title) LIKE lower(?)) OR
-     (lower(projects.content) LIKE lower(?))', "%#{search}%", "%#{search}%")
+     (lower(projects.content) LIKE lower(?)) OR
+     (lower(tags.name) LIKE lower(?))' , "%#{search}%", "%#{search}%", "%#{search}%")
     else
       Project.joins(
         'LEFT OUTER JOIN "likes" ON "likes"."project_id" = "projects"."id"
