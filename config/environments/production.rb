3dc73d43713d0ab307c11cae669cc710667e711d
Chike Abuah <abuahchu@gmail.com>
2017-04-21 21:29:32 -0400

    revert bcuz cutover

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,7 @@ Rsense::Application.configure do
   if `hostname`.chomp == 'rsense-dev.cs.uml.edu'
     config.action_mailer.default_url_options = { host: `hostname`.chomp }
   else
-    # TODO: revert upon cutover
-    config.action_mailer.default_url_options = { protocol: 'http', host: 'rsense-beanstalk-env.j344zpn2wr.us-east-1.elasticbeanstalk.com' }
+    config.action_mailer.default_url_options = { protocol: 'http', host: 'isenseproject.org' }
   end
 
   # unset X-FRAME
