1c1b37db1decb5a66913ea1feb87a79ef6ae73e8
doug-salvati <dougiesal@gmail.com>
2017-08-15 10:38:04 -0400

    Set email preference on acct creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403144606) do
+ActiveRecord::Schema.define(version: 20170814213517) do
 
   create_table "contrib_keys", force: true do |t|
     t.string   "name",       null: false
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(version: 20170403144606) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "subscribed"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
