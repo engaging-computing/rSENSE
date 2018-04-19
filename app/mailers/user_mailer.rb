2e4e43c91319fedf94f02d824aa3109c63467d02
doug-salvati <dougiesal@gmail.com>
2018-04-19 11:06:25 -0400

    Remove vestigial validation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,16 +3,6 @@ require 'base64'
 class UserMailer < ActionMailer::Base
   helper ActionView::Helpers::UrlHelper
 
-  def validation_email(user)
-    @user = user
-    @url  = url_for(controller: 'users', action: 'validate', key: @user.validation_key, only_path: false)
-
-    hostname = `hostname`.chomp
-    from = "no-reply@#{hostname}"
-
-    mail(from: from, to: user.email, subject: "Please verify your iSENSE account's email.")
-  end
-
   def pw_reset_email(user)
     user.send_reset_password_instructions
   end
