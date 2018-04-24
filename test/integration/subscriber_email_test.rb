e2ca233e67470cbde04fe75af704dce083e79080
Dayyvid <davidnguyencollege@gmail.com>
2018-04-23 20:01:03 -0400

    Tests for subscriber emails

--- test/integration/subscriber_email_test.rb
+++ test/integration/subscriber_email_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 require_relative 'base_integration_test'
 
-class ShowSubscriberEmailTest < IntegrationTest
+class SubscriberEmailTest < IntegrationTest
   self.use_transactional_fixtures = false
 
   # testing subscriber_emails#index
