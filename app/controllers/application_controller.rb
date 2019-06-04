b8839630e291f6b53436d84ff206060459a197c5
christopher <christopher.r.odom@gmail.com>
2019-06-04 13:05:28 -0400

    Updated 'for' to 'permit'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -219,9 +219,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :bio, :admin, :subscribed) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :bio, :admin, :subscribed) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :bio, :admin, :subscribed) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :bio, :admin, :subscribed) }
   end
 end
 
