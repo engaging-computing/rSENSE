9018e328c354f98d561fe6601940314042a59241
shrdlu <git@rk.blueeyedzoo.com>
2024-03-06 22:18:35 -0600

    removed unneeded comments

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,6 @@ config.each do |key, value|
   ENV[key] = value
 end
 
-#   # Load the Rails application
-#   require File.expand_path('application', __dir__)
-
 
 require File.expand_path('../boot', __FILE__)
 
