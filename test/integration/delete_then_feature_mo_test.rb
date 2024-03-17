bba2788b00256b577fb7bb6870857584c5f457a6
nehabhosale011996 <nehabhosale011996@gmail.com>
2024-03-17 14:46:53 -0500

    Remove test case

--- test/integration/delete_then_feature_mo_test.rb
+++ test/integration/delete_then_feature_mo_test.rb
@@ -49,13 +49,6 @@ class DeleteThenFeatureMoTest < IntegrationTest
     find(:css, '#template_file_form').attach_file('file', csv_path)
     find(:css, 'button.btn-primary').click
 
-    # actually save the visualization
-    find('#save-ctrls > .vis-ctrl-header').click
-    click_on 'Save Visualization'
-    click_on 'Finish'
-    assert page.has_content?('Visualization was successfully created.'),
-      'Visualization should have been created'
-
     # upload a media object to the project
     url = page.current_path
     img_path = Rails.root.join('test', 'CSVs', 'nerdboy.jpg')
