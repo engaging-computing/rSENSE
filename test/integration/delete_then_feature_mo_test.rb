e69ef0c3091d274d14da14229112705eada053b0
nehabhosale011996 <nehabhosale011996@gmail.com>
2024-03-17 22:12:29 -0500

    change selector for save vis button

--- test/integration/delete_then_feature_mo_test.rb
+++ test/integration/delete_then_feature_mo_test.rb
@@ -49,9 +49,11 @@ class DeleteThenFeatureMoTest < IntegrationTest
     find(:css, '#template_file_form').attach_file('file', csv_path)
     find(:css, 'button.btn-primary').click
 
+    sleep 3
     # actually save the visualization
-    find(:css, '#save-ctrls > .vis-ctrl-header', wait: 10).click
-    click_on 'Save Visualization'
+    assert page.has_content?('Save Visualization')
+    find('#save-vis-btn').click
+    # click_on 'Save Visualization'
     click_on 'Finish'
     assert page.has_content?('Visualization was successfully created.'),
       'Visualization should have been created'
