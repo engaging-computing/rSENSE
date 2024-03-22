96cfc7e806cac89025844c18fda1678202f81dc2
nehabhosale011996 <nehabhosale011996@gmail.com>
2024-03-22 17:59:12 -0500

    checking current url

--- test/integration/delete_then_feature_mo_test.rb
+++ test/integration/delete_then_feature_mo_test.rb
@@ -49,10 +49,21 @@ class DeleteThenFeatureMoTest < IntegrationTest
     find(:css, '#template_file_form').attach_file('file', csv_path)
     find(:css, 'button.btn-primary').click
 
-    sleep 3
+    # Assuming you are within a Capybara test context
+    # This will output the current URL to the console
+    puts current_url
+
+    # You can also assign it to a variable if you need to perform further checks
+    current_url = page.current_url
+
+    expected_url_regex = %r{/projects/\d+/data_sets/\d+}
+
+    # Asserting that the current URL matches the expected URL pattern
+    assert_match expected_url_regex, current_url
+        
     # actually save the visualization
-    find('#save-vis-btn').click
-    # click_on 'Save Visualization'
+    find(:css, '#save-ctrls > .vis-ctrl-header', wait: 10).click
+    click_on 'Save Visualization'
     click_on 'Finish'
     assert page.has_content?('Visualization was successfully created.'),
       'Visualization should have been created'
